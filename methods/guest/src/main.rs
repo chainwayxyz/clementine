c0dc4baf57bc05c45087f14c61e94282f74c7d6f
Ekrem BAL <mail.ekrembal@gmail.com>
2023-12-12 14:18:45 +0300

    init

---
+++ methods/guest/src/main.rs
@@ -0,0 +1,20 @@
+#![no_main]
+// If you want to try std support, also update the guest Cargo.toml file
+#![no_std]  // std support is experimental
+
+
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+pub fn main() {
+    // TODO: Implement your guest code here
+
+    // read the input
+    let input: u32 = env::read();
+
+    // TODO: do something with the input
+
+    // write public output to the journal
+    env::commit(&input);
+}
