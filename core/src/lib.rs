7e4e7dfcc1ab3de80d3f701898d17246995b925c
Hakan Karakuş <hakan.karakus@boun.edu.tr>
2023-12-14 14:40:17 +0300

    work calculation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,47 +1,4 @@
 #![no_main]
 #![no_std]
+
 pub mod btc;
-use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
-
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct BlockHeader {
-    pub version: [u8; 4],
-    pub previous_block_hash: [u8; 32],
-    pub merkle_root: [u8; 32],
-    pub timestamp: [u8; 4],
-    pub bits: [u8; 4],
-    pub nonce: [u8; 4],
-}
-
-impl BlockHeader {
-    pub fn from_slice(input: &[u8; 80]) -> Self {
-        BlockHeader {
-            version: input[0..4].try_into().unwrap(),
-            previous_block_hash: input[4..36].try_into().unwrap(),
-            merkle_root: input[36..68].try_into().unwrap(),
-            timestamp: input[68..72].try_into().unwrap(),
-            bits: input[72..76].try_into().unwrap(),
-            nonce: input[76..80].try_into().unwrap(),
-        }
-    }
-
-    pub fn as_bytes(&self) -> [u8; 80] {
-        let mut output: [u8; 80] = [0; 80];
-        output[0..4].copy_from_slice(&self.version);
-        output[4..36].copy_from_slice(&self.previous_block_hash);
-        output[36..68].copy_from_slice(&self.merkle_root);
-        output[68..72].copy_from_slice(&self.timestamp);
-        output[72..76].copy_from_slice(&self.bits);
-        output[76..80].copy_from_slice(&self.nonce);
-        output
-    }
-}
-
-pub fn calculate_double_sha256(input: &[u8]) -> [u8; 32] {
-    let mut hasher = Sha256::new();
-    hasher.update(input);
-    let result = hasher.finalize_reset();
-    hasher.update(result);
-    hasher.finalize().try_into().unwrap()
-}
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,7 +2,7 @@
 // If you want to try std support, also update the guest Cargo.toml file
 #![no_std] // std support is experimental
 
-use bridge_core::{calculate_double_sha256, BlockHeader};
+use bridge_core::btc::{calculate_double_sha256, BlockHeader};
 use risc0_zkvm::guest::env;
 risc0_zkvm::guest::entry!(main);
 
