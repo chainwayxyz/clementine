2ea43d6e88e63d411e1619232d3e0dbea8577e9e
Ekrem BAL <mail.ekrembal@gmail.com>
2023-12-14 11:20:55 +0300

    Works

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,44 @@
+#![no_main]
+#![no_std]
+
+use risc0_zkvm::sha::{Impl, Sha256};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct Example {
-    pub field: String,
+pub struct BlockHeader {
+    pub version: [u8; 4],
+    pub previous_block_hash: [u8; 32],
+    pub merkle_root: [u8; 32],
+    pub timestamp: [u8; 4],
+    pub bits: [u8; 4],
+    pub nonce: [u8; 4],
+}
+
+pub fn parse_bytes_to_blockheader(input: &[u8; 80]) -> BlockHeader {
+    BlockHeader {
+        version: input[0..4].try_into().unwrap(),
+        previous_block_hash: input[4..36].try_into().unwrap(),
+        merkle_root: input[36..68].try_into().unwrap(),
+        timestamp: input[68..72].try_into().unwrap(),
+        bits: input[72..76].try_into().unwrap(),
+        nonce: input[76..80].try_into().unwrap(),
+    }
+}
+
+pub fn blockheader_to_bytes(input: &BlockHeader) -> [u8; 80] {
+    let mut output: [u8; 80] = [0; 80];
+    output[0..4].copy_from_slice(&input.version);
+    output[4..36].copy_from_slice(&input.previous_block_hash);
+    output[36..68].copy_from_slice(&input.merkle_root);
+    output[68..72].copy_from_slice(&input.timestamp);
+    output[72..76].copy_from_slice(&input.bits);
+    output[76..80].copy_from_slice(&input.nonce);
+    output
+}
+
+pub fn calculate_double_sha256(input: &[u8]) -> [u8; 32] {
+    Impl::hash_bytes(Impl::hash_bytes(&input).as_bytes())
+        .as_bytes()
+        .try_into()
+        .unwrap()
 }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,26 +2,14 @@
 // If you want to try std support, also update the guest Cargo.toml file
 #![no_std] // std support is experimental
 
+use bridge_core::{calculate_double_sha256, BlockHeader, blockheader_to_bytes};
 use risc0_zkvm::guest::env;
 use risc0_zkvm::sha::{Impl, Sha256};
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    // read the input
-    let input1: [u8; 8] = env::read();
-    let input2: [u8; 32] = env::read();
-    let input3: [u8; 32] = env::read();
-    let input4: [u8; 8] = env::read();
-
-    let mut data: [u8; 80] = [0; 80];
-    data[0..8].copy_from_slice(&input1);
-    data[8..40].copy_from_slice(&input2);
-    data[40..72].copy_from_slice(&input3);
-    data[72..80].copy_from_slice(&input4);
-
-    let digest = Impl::hash_bytes(Impl::hash_bytes(&data).as_bytes());
-    let digest_as_bytes: [u8; 32] = digest.as_bytes().try_into().unwrap();
-
-    // write public output to the journal
-    env::commit(&digest_as_bytes);
+    let block_header: BlockHeader = env::read();
+    let data = blockheader_to_bytes(&block_header);
+    let block_hash = calculate_double_sha256(&data);
+    env::commit(&block_hash);
 }
