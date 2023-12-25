545a401bcc77ca94cb6c936b356a1293455f3e0a
Hakan Karakuş <hakan.karakus@boun.edu.tr>
2023-12-25 14:16:03 +0300

    convert functions to method calls on merkle tree

--- methods/guest/src/merkle.rs
+++ methods/guest/src/merkle.rs
@@ -1,18 +1,6 @@
 use bridge_core::incremental_merkle::{IncrementalMerkleTree, Data, DEPTH, sha256};
 use risc0_zkvm::guest::env;
 
-pub fn add_to_incremental_merkle_tree(mut merkle_tree_data: IncrementalMerkleTree, leaf: Data) {
-    merkle_tree_data.add(leaf);
-}
-
-pub fn get_incremental_merkle_tree_root(merkle_tree_data: IncrementalMerkleTree) -> Data {
-    merkle_tree_data.root
-}
-
-pub fn get_incremental_merkle_tree_index(merkle_tree_data: IncrementalMerkleTree) -> u32 {
-    merkle_tree_data.index
-}
-
 pub fn verify_incremental_merkle_path(merkle_tree_data: IncrementalMerkleTree, index: u32) -> Data {
     let leaf: Data = env::read();
     let mut hash: Data = leaf;
