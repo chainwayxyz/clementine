afe46d670998cf34cc6606ce835cb7a280f2833f
ozankaymak <ozan.kaymak@boun.edu.tr>
2023-12-28 18:14:21 +0300

    Add tests, change merkle tree

--- circuits/host/src/bitcoin.rs
+++ circuits/host/src/bitcoin.rs
@@ -50,131 +50,81 @@ pub fn from_json_to_host_tx(file_path: &str) -> HostTransaction {
     return tx_json.into();
 }
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct Node {
-    data: [u8; 32], // Placeholder for the hash
-    level: u8,
-    index: u32,
-}
-
-impl Node {
-    fn new(data: [u8; 32], level: u8, index: u32) -> Self {
-        Node { data, level: level, index: index}
-    }
-
-    pub fn get_data(&self) -> [u8; 32] {
-        return self.data;
-    }
-
-    pub fn get_index(&self) -> u32 {
-        return self.index;
-    }
-    pub fn get_level(&self) -> u8 {
-        return self.level;
-    }
-}
-
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BitcoinMerkleTree {
-    depth: u8,
-    nodes: Vec<Node>,
-    number_of_transactions: u32,
-    number_of_elems_per_level: Vec<u32>,
+    depth: u32,
+    nodes: Vec<Vec<[u8; 32]>>,
 }
 
 impl BitcoinMerkleTree {
-    pub fn new(depth: u8, transactions: Vec<[u8; 32]>, number_of_txs: u32) -> Self {
+    pub fn new(depth: u32, transactions: Vec<[u8; 32]>) -> Self {
         assert!(depth > 0, "Depth must be greater than 0");
         assert!(depth <= 254, "Depth must be less than or equal to 254");
-        assert!(u32::pow(2, (depth) as u32) >= number_of_txs, "Too many transactions for this depth");
-        assert!(number_of_txs == transactions.len() as u32, "Number of transactions does not match the length of the transactions array");
+        assert!(u32::pow(2, (depth) as u32) >= transactions.len() as u32, "Too many transactions for this depth");
         let mut tree = BitcoinMerkleTree {
             depth: depth,
             nodes: vec![],
-            number_of_transactions: number_of_txs,
-            number_of_elems_per_level: vec![],
         };
 
         // Populate leaf nodes
-        for (i, tx) in transactions.iter().enumerate() {
-            let tx_clone = tx.clone();
-            tree.nodes.push(Node::new(tx_clone, 0, i as u32));
+        tree.nodes.push(vec![]);
+        for tx in transactions.iter() {
+            tree.nodes[0].push(*tx);
         }
-        tree.number_of_elems_per_level.push(number_of_txs);
+        println!("tree.nodes[0] length: {:?}", tree.nodes[0].len());
 
         // Construct the tree
-        let mut curr_level_offset: u8 = 1;
+        let mut curr_level_offset: usize = 1;
         let mut prev_level_size = transactions.len();
         let mut prev_level_index_offset = 0;
-        let mut curr_level_index_offset = transactions.len();
-
+        let mut preimage: [u8; 64] = [0; 64];
         while prev_level_size > 1 {
+            // println!("curr_level_offset: {}", curr_level_offset);
+            // println!("prev_level_size: {}", prev_level_size);
+            // println!("prev_level_index_offset: {}", prev_level_index_offset);
+            tree.nodes.push(vec![]);
             for i in 0..(prev_level_size / 2) {
-                let mut preimage: [u8; 64] = [0; 64];
-                preimage[..32].copy_from_slice(&tree.nodes[prev_level_index_offset + i * 2].data);
-                preimage[32..].copy_from_slice(&tree.nodes[prev_level_index_offset + i * 2 + 1].data);
+                preimage[..32].copy_from_slice(&tree.nodes[curr_level_offset - 1 as usize][prev_level_index_offset + i * 2]);
+                preimage[32..].copy_from_slice(&tree.nodes[curr_level_offset - 1][prev_level_index_offset + i * 2 + 1]);
                 let combined_hash = calculate_double_sha256(&preimage);
-                tree.nodes.push(Node::new(combined_hash, curr_level_offset as u8, i as u32));
+                tree.nodes[curr_level_offset].push(combined_hash);
             }
             if prev_level_size % 2 == 1 {
                 let mut preimage: [u8; 64] = [0; 64];
-                preimage[..32].copy_from_slice(&tree.nodes[prev_level_index_offset + prev_level_size - 1].data);
-                preimage[32..].copy_from_slice(&tree.nodes[prev_level_index_offset + prev_level_size - 1].data);
+                preimage[..32].copy_from_slice(&tree.nodes[curr_level_offset - 1][prev_level_index_offset + prev_level_size - 1]);
+                preimage[32..].copy_from_slice(&tree.nodes[curr_level_offset - 1][prev_level_index_offset + prev_level_size - 1]);
                 let combined_hash = calculate_double_sha256(&preimage);
-                tree.nodes.push(Node::new(combined_hash, curr_level_offset as u8, (prev_level_size / 2) as u32));
+                tree.nodes[curr_level_offset].push(combined_hash);
             }
             curr_level_offset += 1;
             prev_level_size = (prev_level_size + 1) / 2;
-            prev_level_index_offset = curr_level_index_offset;
-            curr_level_index_offset += prev_level_size;
-            tree.number_of_elems_per_level.push(prev_level_size as u32);
+            prev_level_index_offset = 0;
         }
         tree
     }
 
     // Returns the Merkle root
     pub fn root(&self) -> [u8; 32] {
-        return self.nodes.last().unwrap().data;
+        return self.nodes[self.nodes.len() - 1][0];
     }
 
-    pub fn get_root_index(&self) -> u32 {
-        return self.nodes.last().unwrap().index;
+    pub fn get_element(&self, level: u32, index: u32) -> [u8; 32] {
+        return self.nodes[level as usize][index as usize];
     }
 
-    pub fn get_element(&self, level: u8, index: u32) -> Node {
-        let mut no_of_tx = self.number_of_transactions;
-        let mut idx = 0;
-        let mut i = 0;
-        while i < level {
-            idx += no_of_tx;
-            no_of_tx = (no_of_tx + 1) / 2;
-            i += 1;
-        }
-        return self.nodes[(idx + index) as usize];
-    }
-
-    pub fn get_element_from_index(&self, index: u32) -> Node {
-        return self.nodes[index as usize];
-    }
-
-    pub fn get_no_of_elem_arr(&self) -> Vec<u32> {
-        return self.number_of_elems_per_level.clone();
-    }
-
-    pub fn get_tx_id_path(&self, index: u32) -> Vec<Node> {
-        assert!(index < self.number_of_transactions, "Index out of bounds");
-        let mut path: Vec<Node> = vec![];
+    pub fn get_idx_path(&self, index: u32) -> Vec<[u8; 32]> {
+        assert!(index <= self.nodes[0].len() as u32 - 1, "Index out of bounds");
+        let mut path = vec![];
+        let mut level = 0;
         let mut i = index;
-        let mut level: u8 = 0;
-        while level < self.depth {
+        while level < self.nodes.len() as u32 - 1 {
             if i % 2 == 1 {
-                path[level as usize] = self.get_element(level, i - 1);
+                path.push(self.nodes[level as usize][i as usize - 1]);
             } else {
-                if (self.number_of_elems_per_level[level as usize] - 1) == i {
-                    path[level as usize] = self.get_element(level, i);
+                if (self.nodes[level as usize].len() - 1) as u32 == i {
+                    path.push(self.nodes[level as usize][i as usize]);
                 } else {
-                    path[level as usize] = self.get_element(level, i + 1);
+                    path.push(self.nodes[level as usize][(i + 1) as usize]);
                 }
             }
             level += 1;
@@ -183,25 +133,48 @@ impl BitcoinMerkleTree {
         return path;
     }
 
-    pub fn calculate_root_with_merkle_proof(&self, tx_id: [u8; 32], merkle_proof: Vec<Node>) -> [u8; 32] {
+    pub fn verify_tx_merkle_proof(&self, idx: u32) {
+        let tx_id = self.nodes[0][idx as usize];
+        let merkle_proof = self.get_idx_path(idx);
         let mut preimage: [u8; 64] = [0; 64];
         let mut combined_hash: [u8; 32] = tx_id.clone();
-        let mut level: u8 = 0;
+        let mut index = idx;
+        let mut level: u32 = 0;
         while level < self.depth {
-            if merkle_proof[level as usize].index % 2 == 1 {
+            if index % 2 == 0 {
                 preimage[..32].copy_from_slice(&combined_hash);
-                preimage[32..].copy_from_slice(&merkle_proof[level as usize].data);
+                preimage[32..].copy_from_slice(&merkle_proof[level as usize]);
                 combined_hash = calculate_double_sha256(&preimage);
             } else {
-                preimage[..32].copy_from_slice(&merkle_proof[level as usize].data);
+                preimage[..32].copy_from_slice(&merkle_proof[level as usize]);
                 preimage[32..].copy_from_slice(&combined_hash);
                 combined_hash = calculate_double_sha256(&preimage);
             }
             level += 1;
+            index = index / 2;
         }
-        combined_hash.reverse();
-        return combined_hash;
+        assert_eq!(combined_hash, self.root());
     }
+
+    // pub fn calculate_root_with_merkle_proof(&self, tx_id: [u8; 32], merkle_proof: Vec<Node>) -> [u8; 32] {
+    //     let mut preimage: [u8; 64] = [0; 64];
+    //     let mut combined_hash: [u8; 32] = tx_id.clone();
+    //     let mut level: u8 = 0;
+    //     while level < self.depth {
+    //         if merkle_proof[level as usize].index % 2 == 1 {
+    //             preimage[..32].copy_from_slice(&combined_hash);
+    //             preimage[32..].copy_from_slice(&merkle_proof[level as usize].data);
+    //             combined_hash = calculate_double_sha256(&preimage);
+    //         } else {
+    //             preimage[..32].copy_from_slice(&merkle_proof[level as usize].data);
+    //             preimage[32..].copy_from_slice(&combined_hash);
+    //             combined_hash = calculate_double_sha256(&preimage);
+    //         }
+    //         level += 1;
+    //     }
+    //     combined_hash.reverse();
+    //     return combined_hash;
+    // }
 
 }
 
@@ -291,8 +264,11 @@ mod tests {
             tx_id_bytes.reverse();
             tx_id_bytes.try_into().unwrap()
         }).collect();
-        let merkle_tree = BitcoinMerkleTree::new(12, tx_vec.clone(), tx_vec.len() as u32);
-        println!("{:?}", hex::encode(merkle_tree.root()));
+        println!("{:?}", tx_vec);
+        let merkle_tree = BitcoinMerkleTree::new(12, tx_vec.clone());
+        for i in 0..tx_vec.len() {
+            merkle_tree.verify_tx_merkle_proof(i as u32);
+        }
     }
 
     #[test]
