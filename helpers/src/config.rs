6f37ea51cdcfb2627ce3c894e2fe1cf61445c5f8
ozankaymak <ozan.kaymak@boun.edu.tr>
2024-01-12 11:20:22 +0300

    Add test

--- helpers/src/config.rs
+++ helpers/src/config.rs
@@ -25,6 +25,7 @@ pub const USER_TAKES_AFTER: u32 = 200;
 pub const FED_TAKES_AFTER: u32 = 1000;
 pub const REGTEST: Network = bitcoin::Network::Regtest;
 pub const NUM_VERIFIERS: usize = 10;
+pub const NUM_USERS: usize = 10;
 pub const MIN_RELAY_FEE: u64 = 445;
 pub const BRIDGE_AMOUNT_SATS: u64 = 100_000_000;
 pub const NUM_ROUNDS: usize = 10;
--- operator/src/main.rs
+++ operator/src/main.rs
@@ -12,10 +12,11 @@ use bitcoin::{
     XOnlyPublicKey,
 };
 use bitcoincore_rpc::{Auth, Client, RpcApi};
+use circuit_helpers::config::{BRIDGE_AMOUNT_SATS, NUM_ROUNDS, NUM_USERS, NUM_VERIFIERS};
 use operator::{
-    actor::Actor, transactions::tx_deposit, proof::withdrawals::pay_withdrawals, operator::Operator, verifier::Verifier, user::User,
+    actor::Actor, operator::Operator, proof::withdrawals::pay_withdrawals,
+    transactions::tx_deposit, user::User, verifier::Verifier,
 };
-use circuit_helpers::config::{NUM_VERIFIERS, BRIDGE_AMOUNT_SATS};
 
 pub fn f() {
     let rpc = Client::new(
@@ -46,35 +47,47 @@ pub fn f() {
     // do_tx(&rpc, tx_d);
 }
 
-
-
 fn main() {
+    let mut bridge_funds: Vec<bitcoin::Txid> = Vec::new();
     let rpc = Client::new(
         "http://localhost:18443/wallet/admin",
         Auth::UserPass("admin".to_string(), "admin".to_string()),
     )
     .unwrap_or_else(|e| panic!("Failed to connect to Bitcoin RPC: {}", e));
     let mut operator = Operator::new(&mut OsRng, &rpc);
-    let user = User::new(&mut OsRng, &rpc);
+
+    let mut users = Vec::new();
+    for _ in 0..NUM_USERS {
+        users.push(User::new(&mut OsRng, &rpc));
+    }
     let mut verifiers = operator.verifiers.clone();
     verifiers.push(operator.signer.xonly_public_key.clone());
 
     let mut verifiers_evm_addresses = operator.verifier_evm_addresses.clone();
     verifiers_evm_addresses.push(operator.signer.evm_address);
 
-    let (utxo, hash, return_address) =
-        user.deposit_tx(&user.rpc, BRIDGE_AMOUNT_SATS, &user.secp, verifiers);
-    rpc.generate_to_address(1, &operator.signer.address)
-        .unwrap();
-    let signatures = operator.new_deposit(utxo, hash, return_address, user.signer.evm_address);
+    let mut utxos = Vec::new();
+    let mut return_addresses = Vec::new();
 
-    let fund = operator.preimage_revealed(user.reveal_preimage(), utxo, return_address);
-    println!("fund: {:?}", fund);
-    operator.move_single_bridge_fund( utxo.txid, utxo);
+    for i in 0..NUM_USERS {
+        let user = &users[i];
+        let (mut utxo, mut hash, mut return_address) =
+            user.deposit_tx(&user.rpc, BRIDGE_AMOUNT_SATS, &user.secp, verifiers.clone());
+        bridge_funds.push(utxo.txid);
+        return_addresses.push(return_address);
+        utxos.push(utxo);
+        rpc.generate_to_address(1, &operator.signer.address)
+            .unwrap();
+        let signatures = operator.new_deposit(utxo, hash, return_address, user.signer.evm_address);
+    }
+
+    let mut fund = operator.preimage_revealed(users[9].reveal_preimage(), utxos[9], return_addresses[9]);
+    for i in 0..NUM_ROUNDS {
+        fund = operator.move_single_bridge_fund(utxos[9].txid, fund);
+        println!("fund moving in round {i}: {:?}", fund);
+    }
     // TEST IF SIGNATURES ARE VALID
     // operator.preimage_revealed(preimage, txid);
-
-    let bridge_funds: Vec<bitcoin::Txid> = Vec::new();
 
     // let (block_hash_vec, deposit_txs, withdrawal_addresses) =
     //     mock_lightclient(&mut OsRng, &rpc, verifiers, 100, 5, 3);
@@ -82,5 +95,4 @@ fn main() {
     // println!("Hello, world!");
 
     // let withdrawal_block_hash = pay_withdrawals(&rpc, withdrawal_addresses);
-
 }
