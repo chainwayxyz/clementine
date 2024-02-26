3d08a0e450c249bea7f4cb1e3c9305c49a3011a7
Hakkush-07 <hakan.karakus@boun.edu.tr>
2024-02-26 14:38:53 +0300

    fix merge errors

--- helpers/src/bridge.rs
+++ helpers/src/bridge.rs
@@ -2,7 +2,7 @@ use crypto_bigint::U256;
 
 use crate::{
     bitcoin::{read_and_verify_bitcoin_merkle_path, read_arbitrary_tx_and_calculate_txid},
-    config::{BRIDGE_AMOUNT_SATS, NUM_ROUNDS},
+    config::{BRIDGE_AMOUNT_SATS, DEPTH, NUM_ROUNDS},
     constant::DUST_VALUE,
     env::Environment,
     incremental_merkle::IncrementalMerkleTree,
@@ -13,20 +13,20 @@ use crate::{
 /// adds blockhashes to an incremental merkle tree.
 /// returns: total work accumulated, blockhash at N - MAX_BLOCK_HANDLE_OPS, blockhash at N
 pub fn read_blocks_and_add_to_merkle_tree<E: Environment>(
-    start_prev_block_hash: [u8; 32],
-    imt: &mut IncrementalMerkleTree,
+    _start_prev_block_hash: [u8; 32],
+    _imt: &mut IncrementalMerkleTree<DEPTH>,
 ) -> (U256, [u8; 32], [u8; 32]) {
     unimplemented!()
 }
 
-pub fn read_blocks_and_calculate_work<E: Environment>(start_prev_block_hash: [u8; 32]) -> U256 {
+pub fn read_blocks_and_calculate_work<E: Environment>(_start_prev_block_hash: [u8; 32]) -> U256 {
     unimplemented!()
 }
 
 // Reads a merkle tree proof, adds output address to incremental merkle tree, merkle tree depth is D
 pub fn read_merkle_tree_proof<E: Environment, const D: usize>(
-    leaf: [u8; 32],
-    index: Option<u32>,
+    _leaf: [u8; 32],
+    _index: Option<u32>,
 ) -> [u8; 32] {
     unimplemented!()
 }
@@ -34,7 +34,7 @@ pub fn read_merkle_tree_proof<E: Environment, const D: usize>(
 /// Reads a withdrawal proof, adds output address to incremental merkle tree
 pub fn read_withdrawal_proof<E: Environment>(
     block_mt_root: [u8; 32],
-    imt: &mut IncrementalMerkleTree,
+    imt: &mut IncrementalMerkleTree<DEPTH>,
 ) {
     let output_address = E::read_32bytes();
     let txid =
@@ -52,8 +52,8 @@ pub fn read_preimages_and_calculate_commit_taproot<E: Environment>() -> ([u8; 32
 }
 
 pub fn read_and_verify_lc_proof<E: Environment>(
-    lc_blockhash: [u8; 32],
-    withdrawal_mt_root: [u8; 32],
+    _lc_blockhash: [u8; 32],
+    _withdrawal_mt_root: [u8; 32],
 ) {
 }
 
@@ -61,14 +61,14 @@ pub fn read_and_verify_verifiers_challenge_proof<E: Environment>() -> (U256, [u8
     unimplemented!()
 }
 
-pub const start_blockhash: [u8; 32] = [0; 32];
-pub const periods_claim_mt_roots: [[u8; 32]; NUM_ROUNDS] = [[0; 32]; NUM_ROUNDS];
+pub const START_BLOCKHASH: [u8; 32] = [0; 32];
+pub const PERIODS_CLAIM_MT_ROOTS: [[u8; 32]; NUM_ROUNDS] = [[0; 32]; NUM_ROUNDS];
 
 pub fn bridge_proof<E: Environment>() {
-    let mut blockhashes_mt = IncrementalMerkleTree::initial();
-    let mut withdrawal_mt = IncrementalMerkleTree::initial();
+    let mut blockhashes_mt = IncrementalMerkleTree::new();
+    let mut withdrawal_mt = IncrementalMerkleTree::new();
     let mut total_pow = U256::ZERO;
-    let mut last_block_hash = start_blockhash;
+    let mut last_block_hash = START_BLOCKHASH;
     for i in 0..NUM_ROUNDS {
         let (work, lc_blockhash, cur_block_hash) =
             read_blocks_and_add_to_merkle_tree::<E>(last_block_hash, &mut blockhashes_mt);
@@ -96,14 +96,14 @@ pub fn bridge_proof<E: Environment>() {
                 read_merkle_tree_proof::<E, 32>(blockhash, None)
             );
             assert_eq!(
-                periods_claim_mt_roots[i],
+                PERIODS_CLAIM_MT_ROOTS[i],
                 read_merkle_tree_proof::<E, 32>(claim_proof_tree_leaf, Some(withdrawal_mt.index))
             );
 
             let k_deep_work = read_blocks_and_calculate_work::<E>(cur_block_hash);
             total_pow = total_pow.wrapping_add(&k_deep_work);
 
-            let (verifiers_pow, verifiers_last_finalized_bh, verifiers_last_blockheight) =
+            let (verifiers_pow, verifiers_last_finalized_bh, _verifiers_last_blockheight) =
                 read_and_verify_verifiers_challenge_proof::<E>();
 
             // if our pow is bigger and we have different last finalized block hash, we win
--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -8,6 +8,5 @@ pub mod constant;
 pub mod core_tx;
 pub mod core_utils;
 pub mod env;
-pub mod env;
 pub mod hashes;
 pub mod incremental_merkle;
