df7379f9f7049c9af3d8b869b2e9147c62e5e35a
ozankaymak <ozan.kaymak@boun.edu.tr>
2024-03-20 17:05:09 +0300

    Comment out printlns

--- helpers/src/bridge.rs
+++ helpers/src/bridge.rs
@@ -28,7 +28,7 @@ pub fn read_blocks_and_add_to_merkle_tree<E: Environment>(
     max_block_handle_ops: u32,
 ) -> (U256, [u8; 32], [u8; 32]) {
     let n = E::read_u32();
-    println!("READ n: {:?}", n);
+    // println!("READ n: {:?}", n);
     let mut total_work = U256::ZERO;
     let mut curr_prev_block_hash = start_prev_block_hash;
     let mut lc_block_hash: [u8; 32] = [0; 32];
@@ -209,7 +209,7 @@ pub fn bridge_proof<E: Environment>() {
     let mut total_num_withdrawals = 0;
     let mut last_period = 0;
     for period_count in 0..NUM_ROUNDS {
-        println!("Proving for Period: {}", period_count);
+        // println!("Proving for Period: {}", period_count);
 
         let work;
         // println!("ROUND: {:?}", period_count);
@@ -233,7 +233,7 @@ pub fn bridge_proof<E: Environment>() {
             last_period = period_count;
             break;
         }
-        println!("Proving for Period: {}", period_count);
+        // println!("Proving for Period: {}", period_count);
     }
 
     let (verifiers_pow, verifiers_last_finalized_blockhash, verifiers_challenge_period) =
@@ -279,32 +279,32 @@ pub fn bridge_proof<E: Environment>() {
     // println!("READ and verify lc proof");
     let (commit_taproot_addr, claim_proof_tree_leaf) =
         read_preimages_and_calculate_commit_taproot::<E>();
-    println!(
-        "READ preimages and calculate commit taproot: {:?}",
-        commit_taproot_addr
-    );
+    // println!(
+    //     "READ preimages and calculate commit taproot: {:?}",
+    //     commit_taproot_addr
+    // );
     let commit_taproot_txid =
         read_tx_and_calculate_txid::<E>(None, Some((None, commit_taproot_addr)));
-    println!("READ tx and calculate txid: {:?}", commit_taproot_txid);
+    // println!("READ tx and calculate txid: {:?}", commit_taproot_txid);
     let reveal_txid = read_tx_and_calculate_txid::<E>(Some((commit_taproot_txid, 0)), None);
     // println!("READ tx and calculate txid: {:?}", reveal_txid);
     // INCORRECT LOGIC: read_and_verify_bitcoin_merkle_path returns the merkle root of a block
     let calculated_merkle_root = read_and_verify_bitcoin_merkle_path::<E>(reveal_txid);
-    println!(
-        "READ and verify bitcoin merkle path: {:?}",
-        calculated_merkle_root
-    );
+    // println!(
+    //     "READ and verify bitcoin merkle path: {:?}",
+    //     calculated_merkle_root
+    // );
     let calculated_blockhash =
         read_header_except_root_and_calculate_blockhash::<E>(calculated_merkle_root);
-    println!("calculated_blockhash: {:?}", calculated_blockhash);
+    // println!("calculated_blockhash: {:?}", calculated_blockhash);
 
     assert_eq!(
         blockhashes_mt.root,
         read_merkle_tree_proof::<E, BLOCKHASH_MERKLE_TREE_DEPTH>(calculated_blockhash, None)
     );
 
-    println!("claim_proof_tree_leaf: {:?}", claim_proof_tree_leaf);
-    println!("total_num_withdrawals: {:?}", total_num_withdrawals);
+    // println!("claim_proof_tree_leaf: {:?}", claim_proof_tree_leaf);
+    // println!("total_num_withdrawals: {:?}", total_num_withdrawals);
 
     // println!(
     //     "mtttttttt: {:?}",
@@ -319,5 +319,5 @@ pub fn bridge_proof<E: Environment>() {
         )
     );
 
-    println!("READ and verify claim proof");
+    // println!("READ and verify claim proof");
 }
