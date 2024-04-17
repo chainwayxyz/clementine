ae4a2bb4869af7dc16dcb58a0772ee954b60bf5a
Ekrem BAL <mail.ekrembal@gmail.com>
2024-04-17 15:42:38 +0300

    verifier jsonrpsee parsing

--- core/src/bin/verifier_server.rs
+++ core/src/bin/verifier_server.rs
@@ -7,22 +7,35 @@ use dotenv::dotenv;
 use jsonrpsee::{server::Server, RpcModule};
 use secp256k1::rand::{rngs::StdRng, SeedableRng};
 use secp256k1::XOnlyPublicKey;
+use serde::Deserialize;
+use serde_json::Value;
 use std::str::FromStr;
 use std::{env, net::SocketAddr};
 
-fn load_config() -> (Vec<u8>, Vec<u8>) {
-    dotenv().ok();
-
-    let secret_key = env::var("SECRET_KEY")
-        .expect("SECRET_KEY must be set in .env")
-        .into_bytes();
-
-    // Dummy implementation for all_xonly_pks, replace with actual file reading logic
-    let all_xonly_pks = vec![];
-
-    (all_xonly_pks, secret_key)
+#[derive(Deserialize)]
+struct NewDepositParams {
+    deposit_txid: String,
+    deposit_vout: u32,
+    user_return_xonly_pk: String,
+    user_evm_address: String,
+    operator_address: String,
 }
 
+/// main function to start verifier server
+/// ```bash
+/// curl -X POST http://localhost:3131 -H "Content-Type: application/json" -d '{
+/// "jsonrpc": "2.0",
+/// "method": "new_deposit",
+/// "params": {
+///     "deposit_txid": "4a993d208d3faae29591a92d6e09fcab58d0a74422d45c784ec8f9b6f8e90f98",
+///     "deposit_vout": 6,
+///     "user_return_xonly_pk": "9a857208e280d56d008894e7088a4e059cccc28efed3cab1c06b0cfbe3df3526",
+///     "user_evm_address": "0000000000000000000000000000000000000000",
+///     "operator_address": "tb1qmfcjlvnwv5rzwu8dyj9akrcgu07a50geewsh5g"
+/// },
+/// "id": 1
+/// }'
+/// ```
 #[tokio::main]
 async fn main() {
     let rpc = ExtendedRpc::new();
@@ -43,7 +56,7 @@ async fn main() {
     // Initialization of Verifier, RPC, etc. goes here
 
     let server = Server::builder()
-        .build("127.0.0.1:0".parse::<SocketAddr>().unwrap())
+        .build("127.0.0.1:3131".parse::<SocketAddr>().unwrap())
         .await
         .unwrap();
     let mut module = RpcModule::new(()); // Use appropriate context
@@ -52,19 +65,45 @@ async fn main() {
 
     // Define your RPC methods
     module
-        .register_async_method("new_deposit", move |_params, _ctx| {
+        .register_async_method("new_deposit", move |params, _ctx| {
+            println!("new_deposit called");
+
+            let parsed_params: NewDepositParams =
+                match serde_json::from_value(params.parse().unwrap()).unwrap() {
+                    Value::Object(map) => serde_json::from_value(Value::Object(map)).unwrap(),
+                    _ => panic!("Invalid params"),
+                };
+
+            let start_utxo = OutPoint::new(
+                bitcoin::Txid::from_str(&parsed_params.deposit_txid).expect("Invalid Txid"),
+                parsed_params.deposit_vout,
+            );
+
+            let return_address = XOnlyPublicKey::from_slice(
+                &hex::decode(&parsed_params.user_return_xonly_pk)
+                    .expect("Invalid hex for XOnlyPublicKey"),
+            )
+            .expect("Invalid XOnlyPublicKey");
+
+            let evm_address: EVMAddress = hex::decode(&parsed_params.user_evm_address)
+                .expect("Invalid EVMAddress")
+                .try_into()
+                .expect("Invalid EVMAddress");
+
+            let operator_address = Address::from_str(&parsed_params.operator_address)
+                .expect("Invalid operator_address")
+                .assume_checked();
+
             let verifier_clone = verifier.clone(); // Assuming Verifier is Clone
             async move {
                 // Call the appropriate method on the Verifier instance
                 verifier_clone
                     .new_deposit(
-                        // Pass the required parameters
-                        // Replace with actual values
-                        OutPoint::default(),
-                        &XOnlyPublicKey::from_slice(&[0; 32]).unwrap(),
+                        start_utxo,
+                        &return_address,
                         0,
-                        &[0u8; 20],
-                        &Address::from_str("tb1qg9yq").unwrap().assume_checked(),
+                        &evm_address,
+                        &operator_address,
                     )
                     .await
                     .unwrap();
@@ -72,8 +111,12 @@ async fn main() {
         })
         .unwrap();
     let handle = server.start(module);
+    println!("Listening on {:?}", handle);
 
     // In this example we don't care about doing shutdown so let's it run forever.
     // You may use the `ServerHandle` to shut it down or manage it yourself.
     tokio::spawn(handle.stopped());
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    }
 }
