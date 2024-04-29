9212348717c16763153e53eb441772b808a0cd07
Ceyhun Şen <ceyhuusen@gmail.com>
2024-04-29 13:02:00 +0300

    config: Add unit tests.

--- core/src/config.rs
+++ core/src/config.rs
@@ -10,11 +10,7 @@
 use crate::errors::BridgeError;
 use bitcoin::Network;
 use serde::{Deserialize, Serialize};
-use std::{
-    fs::File,
-    io::{Error, Read},
-    path::PathBuf,
-};
+use std::{fs::File, io::Read, path::PathBuf};
 
 /// This struct can be used to pass information to other parts of the program.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -67,11 +63,10 @@ impl BridgeConfig {
     /// Try to parse a `BridgeConfig` from given TOML formatted string and
     /// generate a `BridgeConfig`.
     pub fn try_parse_from(input: String) -> Result<Self, BridgeError> {
-        if let Ok(result) = toml::from_str::<BridgeConfig>(&input) {
-            return Ok(result);
-        };
-
-        Err(BridgeError::ConfigError(Error::last_os_error().to_string()))
+        match toml::from_str::<BridgeConfig>(&input) {
+            Ok(c) => Ok(c),
+            Err(e) => Err(BridgeError::ConfigError(e.to_string())),
+        }
     }
 }
 
@@ -92,4 +87,114 @@ impl Default for BridgeConfig {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::BridgeConfig;
+    use std::{
+        fs::{self, File},
+        io::Write,
+    };
+
+    /// This needs a prefix for every test function, because of the async nature
+    /// of the tests. I am not going to implement a mutex solution. Just do:
+    /// let filename = "someprefix".to_string() + TESTFILE;
+    pub const TESTFILE: &str = "test.toml";
+
+    #[test]
+    fn parse_from_string() {
+        // In case of a incorrect file content, we should receive an error.
+        let content = "brokenfilecontent";
+        match BridgeConfig::try_parse_from(content.to_string()) {
+            Ok(_) => assert!(false),
+            Err(e) => {
+                println!("{:#?}", e);
+                assert!(true);
+            }
+        };
+
+        let init = BridgeConfig::new();
+        match BridgeConfig::try_parse_from(toml::to_string(&init).unwrap()) {
+            Ok(c) => {
+                println!("{:#?}", c);
+                assert!(true);
+            }
+            Err(_) => {
+                assert!(false);
+            }
+        };
+    }
+
+    #[test]
+    fn parse_from_file() {
+        let filename = "1".to_string() + TESTFILE;
+        let content = "brokenfilecontent";
+        let mut file = File::create(filename.clone()).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+
+        match BridgeConfig::try_parse_file(filename.clone().into()) {
+            Ok(_) => {
+                assert!(false);
+            }
+            Err(e) => {
+                println!("{:#?}", e);
+                assert!(true);
+            }
+        };
+
+        let content = "db_file_path = \"database\"
+        num_verifiers = 4
+        min_relay_fee = 289
+        user_takes_after = 200
+        confirmation_treshold = 1
+        network = \"regtest\"
+        bitcoin_rpc_url = \"http://localhost:18443\"
+        bitcoin_rpc_user = \"admin\"
+        bitcoin_rpc_password = \"admin\"\n";
+        let mut file = File::create(filename.clone()).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+
+        match BridgeConfig::try_parse_file(filename.clone().into()) {
+            Ok(c) => {
+                println!("{:#?}", c);
+                assert!(true);
+            }
+            Err(_) => {
+                assert!(false);
+            }
+        };
+
+        fs::remove_file(filename.clone()).unwrap();
+    }
+
+    #[test]
+    /// Currently, no support for headers.
+    fn parse_from_file_with_headers() {
+        let filename = "2".to_string() + TESTFILE;
+        let content = "[header1]
+        db_file_path = \"database\"
+        num_verifiers = 4
+        min_relay_fee = 289
+        user_takes_after = 200
+
+        [header2]
+        confirmation_treshold = 1
+        network = \"regtest\"
+        bitcoin_rpc_url = \"http://localhost:18443\"
+        bitcoin_rpc_user = \"admin\"
+        bitcoin_rpc_password = \"admin\"\n";
+        let mut file = File::create(filename.clone()).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+
+        match BridgeConfig::try_parse_file(filename.clone().into()) {
+            Ok(c) => {
+                println!("{:#?}", c);
+                assert!(false);
+            }
+            Err(e) => {
+                println!("{:#?}", e);
+                assert!(true);
+            }
+        };
+
+        fs::remove_file(filename).unwrap();
+    }
+}
