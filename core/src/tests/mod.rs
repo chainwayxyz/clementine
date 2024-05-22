954553fc2f771345acedd14b6555f392eb71c7b1
Ceyhun Şen <ceyhuusen@gmail.com>
2024-05-22 15:07:32 +0300

    common_tests: Add crate comment.

--- core/src/tests/mod.rs
+++ core/src/tests/mod.rs
@@ -1,1 +1,6 @@
+//! # Common Test Elements
+//! 
+//! This crate includes some common elements for unit and integration tests of
+//! Clementine. This crate's elements should not be used outside of tests.
+
 pub mod common;
