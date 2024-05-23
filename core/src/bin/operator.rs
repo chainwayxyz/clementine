3a59471e8bc81f4c5b530b5bb86884069af2e449
Ceyhun Şen <ceyhuusen@gmail.com>
2024-05-23 12:04:24 +0300

    bin: New line fixes.

--- core/src/bin/operator.rs
+++ core/src/bin/operator.rs
@@ -4,6 +4,7 @@ use clementine_core::{cli, create_operator_server};
 async fn main() {
     let config = cli::get_configuration();
     let verifier_endpoints = config.verifier_endpoints.clone().unwrap();
+
     create_operator_server(config, verifier_endpoints)
         .await
         .unwrap()
