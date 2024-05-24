a4a35857d600ed45dbecea87cad2807444acd7e9
Ceyhun Şen <ceyhuusen@gmail.com>
2024-05-24 17:42:42 +0300

    scripts: Delete test_preperation script and update other one a little bit.

--- scripts/prepare_database.sh
+++ scripts/prepare_database.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# This file isn't a strict requirement for preparing database. One can take this
-# as a reference.
+# This script isn't a strict requirement for preparing database. One can take
+# this only as a reference.
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Preparing database for $PGDATABASE"
 
-
 dropdb -U $PGUSER $PGDATABASE
 createdb -U $PGUSER -O $PGUSER $PGDATABASE
+
 cat $SCRIPT_DIR/schema.sql | psql -U $PGUSER $PGDATABASE
