9e0d4938ea5de1e4b8fd77a2f48331a39e66a61e
Ceyhun Şen <ceyhuusen@gmail.com>
2024-05-28 18:06:19 +0300

    user: Remove unnecessary pub keywords, add new lines for readability and add poc feature to generate_deposit_proof.

--- core/src/user.rs
+++ core/src/user.rs
@@ -4,40 +4,38 @@ use crate::errors::BridgeError;
 use crate::extended_rpc::ExtendedRpc;
 use crate::transaction_builder::TransactionBuilder;
 use crate::EVMAddress;
-use bitcoin::secp256k1::Secp256k1;
 use bitcoin::Address;
 use bitcoin::OutPoint;
-use bitcoin::Transaction;
 use bitcoin::XOnlyPublicKey;
 use clementine_circuits::constants::BRIDGE_AMOUNT_SATS;
 use secp256k1::SecretKey;
 
 #[derive(Debug)]
 pub struct User {
-    pub rpc: ExtendedRpc,
-    pub secp: Secp256k1<secp256k1::All>,
-    pub signer: Actor,
-    pub transaction_builder: TransactionBuilder,
+    rpc: ExtendedRpc,
+    signer: Actor,
+    transaction_builder: TransactionBuilder,
 }
 
 impl User {
+    /// Creates a new `User`.
     pub fn new(
         rpc: ExtendedRpc,
         all_xonly_pks: Vec<XOnlyPublicKey>,
         sk: SecretKey,
         config: BridgeConfig,
     ) -> Self {
-        let secp = Secp256k1::new();
         let signer = Actor::new(sk, config.network);
+
         let transaction_builder = TransactionBuilder::new(
             all_xonly_pks.clone(),
             config.network,
             config.user_takes_after,
             config.min_relay_fee,
         );
+
         User {
             rpc,
-            secp,
             signer,
             transaction_builder,
         }
@@ -66,13 +64,15 @@ impl User {
             &evm_address,
             BRIDGE_AMOUNT_SATS,
         )?;
+
         Ok(deposit_address)
     }
 
+    #[cfg(poc)]
     pub fn generate_deposit_proof(&self, _move_txid: Transaction) -> Result<(), BridgeError> {
-        // let out = self.rpc.get_spent_tx_out(&deposit_utxo)?;
-        // self.rpc.get_spent_tx_out(outpoint)
-        // merkle_tree::PartialMerkleTree::from_txids(&[move_txid.wtxid()], &[move_txid.txid()]);
+        let out = self.rpc.get_spent_tx_out(&deposit_utxo)?;
+        self.rpc.get_spent_tx_out(outpoint);
+        merkle_tree::PartialMerkleTree::from_txids(&[move_txid.wtxid()], &[move_txid.txid()]);
         Ok(())
     }
 }
