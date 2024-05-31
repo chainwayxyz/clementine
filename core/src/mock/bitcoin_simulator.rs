b3b8ce4a488994966c90c4347ec6b7d9e30b42eb
Ceyhun Şen <ceyhuusen@gmail.com>
2024-05-31 12:04:48 +0300

    bitcoin_simulator: Add initial implementations.

--- core/src/mock/bitcoin_simulator.rs
+++ core/src/mock/bitcoin_simulator.rs
@@ -3,4 +3,58 @@
 //! This module is a wrapper for Bitcoin simulators. It tries to simulate what
 //! `ExtendedRpc` does.
 
-use bitcoin_simulator;
+use crate::errors::BridgeError;
+use crate::traits::bitcoin_rpc::BitcoinRPC;
+use bitcoin::OutPoint;
+use bitcoin::ScriptBuf;
+use bitcoin_simulator::database::Database;
+use std::io::Error;
+
+pub struct BitcoinMockRPC {
+    database: Database,
+}
+
+impl BitcoinMockRPC {
+    /// Creates a new Bitcoin simulator database.
+    ///
+    /// # Panics
+    ///
+    /// Panics if database connection cannot be established.
+    pub fn new() -> Self {
+        let database = Database::connect_temporary_database().unwrap();
+
+        Self { database }
+    }
+}
+
+impl BitcoinRPC for BitcoinMockRPC {
+    fn is_utxo_spent(&self, outpoint: &OutPoint) -> Result<bool, BridgeError> {
+        match self
+            .database
+            .check_if_output_is_spent(outpoint.txid.to_string().as_str(), outpoint.vout)
+        {
+            Ok(r) => Ok(r),
+            Err(e) => Err(BridgeError::DatabaseError(sqlx::Error::Io(Error::other(
+                e.to_string(),
+            )))),
+        }
+    }
+
+    fn check_utxo_address_and_amount(
+        &self,
+        outpoint: &OutPoint,
+        address: &ScriptBuf,
+        amount_sats: u64,
+    ) -> Result<bool, BridgeError> {
+        todo!()
+    }
+
+    fn send_raw_transaction(
+        &self,
+        tx: &bitcoin::Transaction,
+    ) -> Result<bitcoin::Txid, bitcoincore_rpc::Error> {
+        // self.database.insert_transaction_unconditionally(tx);
+
+        todo!()
+    }
+}
--- core/src/traits/bitcoin_rpc.rs
+++ core/src/traits/bitcoin_rpc.rs
@@ -4,14 +4,24 @@
 //! to select between real and mock interface.
 
 use crate::errors::BridgeError;
-use bitcoin::{OutPoint, ScriptBuf};
+use bitcoin::{OutPoint, ScriptBuf, Transaction};
 
 pub trait BitcoinRPC {
+    /// Should check if an UTXO is spent or not.
     fn is_utxo_spent(&self, outpoint: &OutPoint) -> Result<bool, BridgeError>;
+
+    /// Should check if output checked from `outpoint` equals to new output
+    /// created from `address` and `amount_sats`.
     fn check_utxo_address_and_amount(
         &self,
         outpoint: &OutPoint,
         address: &ScriptBuf,
         amount_sats: u64,
     ) -> Result<bool, BridgeError>;
+
+    /// Should send raw transaction to Bitcoin.
+    fn send_raw_transaction(
+        &self,
+        tx: &Transaction,
+    ) -> Result<bitcoin::Txid, bitcoincore_rpc::Error>;
 }
