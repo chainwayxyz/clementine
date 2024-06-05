3b122eadd4fc34f0414ae95d31214f1fa76a0ef6
Ceyhun Şen <ceyhuusen@gmail.com>
2024-06-05 14:51:45 +0300

    mock_rpc: Add some unimplemented RPC functions.

--- core/src/mock/rpc.rs
+++ core/src/mock/rpc.rs
@@ -3,11 +3,21 @@
 //! This crate mocks Bitcoin's RPC interface. Note that it is not a full mock,
 //! rather a simplified mock for testing purposes.
 
-use std::sync::{Arc, Mutex};
 use crate::{traits::rpc::RpcApiWrapper, transaction_builder::TransactionBuilder};
-use bitcoin::{address::NetworkChecked, consensus::encode, Address, Amount, Transaction, TxOut};
+use bitcoin::{
+    address::NetworkChecked, consensus::encode, hashes::Hash, Address, Amount, Network,
+    SignedAmount, Transaction, TxOut, Wtxid, XOnlyPublicKey,
+};
 use bitcoin_simulator::database::Database;
-use bitcoincore_rpc::{json, RpcApi};
+use bitcoincore_rpc::{
+    json::{
+        self, GetRawTransactionResult, GetTransactionResult, GetTransactionResultDetail,
+        GetTransactionResultDetailCategory, WalletTxInfo,
+    },
+    RpcApi,
+};
+use secp256k1::Secp256k1;
+use std::sync::{Arc, Mutex};
 
 /// Mock Bitcoin RPC client for testing.
 pub struct Client {
@@ -93,6 +103,99 @@ impl RpcApi for Client {
         let txid = self.send_raw_transaction(&tx)?;
 
         Ok(txid)
+    }
+
+    fn get_transaction(
+        &self,
+        txid: &bitcoin::Txid,
+        _include_watchonly: Option<bool>,
+    ) -> bitcoincore_rpc::Result<json::GetTransactionResult> {
+        let raw_tx = self
+            .database
+            .lock()
+            .unwrap()
+            .get_transaction(txid.to_string().as_str())
+            .unwrap();
+
+        let res = GetTransactionResult {
+            info: WalletTxInfo {
+                confirmations: 10,
+                blockhash: None,
+                blockindex: None,
+                blocktime: None,
+                blockheight: None,
+                txid: *txid,
+                time: 0,
+                timereceived: 0,
+                bip125_replaceable: json::Bip125Replaceable::Unknown,
+                wallet_conflicts: vec![],
+            },
+            amount: SignedAmount::ZERO,
+            fee: None,
+            details: vec![GetTransactionResultDetail {
+                address: None,
+                category: GetTransactionResultDetailCategory::Send,
+                amount: SignedAmount::ZERO,
+                label: None,
+                vout: 0,
+                fee: None,
+                abandoned: None,
+            }],
+            hex: encode::serialize(&raw_tx),
+        };
+
+        Ok(res)
+    }
+
+    fn get_new_address(
+        &self,
+        _label: Option<&str>,
+        _address_type: Option<json::AddressType>,
+    ) -> bitcoincore_rpc::Result<Address<bitcoin::address::NetworkUnchecked>> {
+        let secp = Secp256k1::new();
+        let xonly_public_key = XOnlyPublicKey::from_slice(&[
+            0x78u8, 0x19u8, 0x90u8, 0xd7u8, 0xe2u8, 0x11u8, 0x8cu8, 0xc3u8, 0x61u8, 0xa9u8, 0x3au8,
+            0x6fu8, 0xccu8, 0x54u8, 0xceu8, 0x61u8, 0x1du8, 0x6du8, 0xf3u8, 0x81u8, 0x68u8, 0xd6u8,
+            0xb1u8, 0xedu8, 0xfbu8, 0x55u8, 0x65u8, 0x35u8, 0xf2u8, 0x20u8, 0x0cu8, 0x4b,
+        ])
+        .unwrap();
+
+        let address = Address::p2tr(&secp, xonly_public_key, None, Network::Regtest)
+            .as_unchecked()
+            .to_owned();
+
+        Ok(address)
+    }
+
+    fn generate_to_address(
+        &self,
+        _block_num: u64,
+        _address: &Address<NetworkChecked>,
+    ) -> bitcoincore_rpc::Result<Vec<bitcoin::BlockHash>> {
+        Ok(vec![])
+    }
+
+    fn get_raw_transaction_info(
+        &self,
+        txid: &bitcoin::Txid,
+        _block_hash: Option<&bitcoin::BlockHash>,
+    ) -> bitcoincore_rpc::Result<json::GetRawTransactionResult> {
+        Ok(GetRawTransactionResult {
+            in_active_chain: None,
+            hex: vec![],
+            txid: *txid,
+            hash: Wtxid::hash(&[0]),
+            size: 0,
+            vsize: 0,
+            version: 0,
+            locktime: 0,
+            vin: vec![],
+            vout: vec![],
+            blockhash: None,
+            confirmations: Some(10),
+            time: None,
+            blocktime: None,
+        })
     }
 }
 
