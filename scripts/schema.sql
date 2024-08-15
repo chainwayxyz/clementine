971836c2d2b5a8640faf66014164deb472e41898
Ekrem BAL <mail.ekrembal@gmail.com>
2024-08-15 17:11:00 +0300

    Update schema

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -3,10 +3,20 @@ begin;
 -- Verifier table for deposit details
 /* This table holds the information related to a deposit. */
 create table deposit_infos (
-    id INTEGER primary key,
-    deposit_outpoint text not null check (deposit_request_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
+    id serial primary key,
+    deposit_outpoint text not null check (deposit_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
     recovery_taproot_address text not null,
     evm_address text not null check (evm_address ~ '^[a-fA-F0-9]{40}'),
+    created_at timestamp not null default now()
+);
+
+-- Verifier table for operator signatures, operator will read from here and send the take tx
+create table operator_take_sigs (
+    id serial primary key,
+    deposit_outpoint text not null check (deposit_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
+    bridge_fund_txout text not null,
+    operator_index int not null,
+    sig text not null,
     created_at timestamp not null default now()
 );
 
@@ -17,7 +27,7 @@ move_commit_tx, the second triple is for move_reveal_tx, and the rest is for ope
 for each operator. Also for each triple, we hold the sig_hash to be signed to prevent reuse
 of the nonces. */ 
 create table nonces (
-    idx INTEGER primary key,
+    idx serial primary key,
     deposit_outpoint text primary key not null check (deposit_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
     pub_nonce text not null check (pub_nonce ~ '^[a-fA-F0-9]{132}'),
     sec_nonce text not null check (sec_nonce ~ '^[a-fA-F0-9]{128}'),
@@ -26,26 +36,56 @@ create table nonces (
     created_at timestamp not null default now()
 )
 
--- Verifier table for kickoff o for deposits
+CREATE OR REPLACE FUNCTION prevent_sig_hash_update()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- If the old value of sig_hash is not NULL and the new value is different, raise an exception
+    IF OLD.sig_hash IS NOT NULL AND NEW.sig_hash IS DISTINCT FROM OLD.sig_hash THEN
+        RAISE EXCEPTION 'sig_hash cannot be updated once it has a value';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create the trigger
+CREATE TRIGGER prevent_sig_hash_update_trigger
+BEFORE UPDATE ON nonces
+FOR EACH ROW
+EXECUTE FUNCTION prevent_sig_hash_update();
+
+-- Verifier table for kickoff for deposits
 /* This table holds the kickoff utxos sent by the operators for each deposit. */
 create table deposit_kickoff_utxos (
-    id INTEGER primary key,
+    id serial primary key,
     deposit_outpoint text not null check (deposit_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
     kickoff_utxo text not null,
     created_at timestamp not null default now()
+    unique (deposit_outpoint, kickoff_utxo)
 );
 
 -- Operator table for kickoff utxo and funding utxo for deposits
 /* This table holds the funding utxos sent by the operators for each deposit. */
-create table deposit_kickoff_funding_outpoints (
-    deposit_outpoint text primary key not null check (deposit_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
-    kickoff_outpoint text not null check (kickoff_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
+create table deposit_kickoff_generator_txs (
+    id serial primary key,
+    txid text not null check (txid ~ '^[a-fA-F0-9]{64}'),
+    raw_signed_tx text not null,
+    num_kickoffs int not null,
+    cur_unused_kickoff_index int not null,
+    funding_txid text not null check (funding_txid ~ '^[a-fA-F0-9]{64}'),
+    created_at timestamp not null default now()
+);
+
+-- Operator table for 
+/* This table holds the kickoff utxos sent by the operators for each deposit. */
+create table operators_kickoff_utxo (
+    deposit_outpoint primary key text not null check (deposit_outpoint ~ '^[a-fA-F0-9]{64}:\\d+'),
+    kickoff_utxo text not null,
     created_at timestamp not null default now()
 );
 
 -- Operator table for funding utxo used for deposits TODO: Change this table
 create table funding_utxos (
-    id INTEGER primary key,
+    id serial primary key,
     funding_utxo text not null,
     created_at timestamp not null default now()
 );
