9d7c2da19b30693f96e2ec0c347508e3383364a1
Ceyhun Şen <ceyhuusen@gmail.com>
2024-09-06 14:34:26 +0300

    cli: Disable short flags for servers.

--- core/src/cli.rs
+++ core/src/cli.rs
@@ -17,14 +17,17 @@ use std::process::exit;
 pub struct Args {
     /// TOML formatted configuration file.
     pub config_file: PathBuf,
+    /// Verbosity level, ranging from 0 (none) to 5 (highest)
+    #[arg(short, long, default_value_t = 0)]
+    pub verbose: u8,
     /// Enable verifier server.
-    #[clap(short, long)]
+    #[clap(long)]
     pub verifier_server: bool,
     /// Enable operator server.
-    #[clap(short, long)]
+    #[clap(long)]
     pub operator_server: bool,
     /// Enable aggregator server.
-    #[clap(short, long)]
+    #[clap(long)]
     pub aggregator_server: bool,
 }
 
