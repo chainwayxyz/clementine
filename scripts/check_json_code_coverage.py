b58c379dc350b950c3f9f984f512c1aaea677f4d
Ceyhun Şen <ceyhuusen@gmail.com>
2024-09-11 18:33:38 +0300

    Line coverage check and report (#293)
    * workflow: Add initial line coverage.
    
    * workflow: Fix code coverage.
    
    * workflow: Update code checks.
    
    * workflow: Renamings.
    
    * workflow: Code coverage to code_checks.
    
    * gitignore: Add lcov.info.
    
    * workflow: Save lines and function coverage percantage.
    
    * scripts: Add line coverage check.
    
    * workflow: Use new script to check line coverage.
    
    * workflow: Use CARGOFLAGS.
    
    * Revert "workflow: Use CARGOFLAGS."
    
    This reverts commit bd9da21eb4ff33a277b60dc4e533c010ba60f165.

---
+++ scripts/check_json_code_coverage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+import json
+
+EXPECTED_LINE_COVER_PERCENTAGE=70
+EXPECTED_FUNCTION_COVER_PERCENTAGE=70
+
+json_file = sys.argv[1]
+print("Reading JSON file", json_file)
+
+contents = open(json_file, "r").read()
+contents = json.loads(contents)
+
+lines_covered = contents["data"][0]["totals"]["lines"]["percent"]
+print("Lines are covered", lines_covered, "percent")
+
+functions_covered = contents["data"][0]["totals"]["functions"]["percent"]
+print("Functions are covered", lines_covered, "percent")
+
+assert EXPECTED_LINE_COVER_PERCENTAGE <= lines_covered
+assert EXPECTED_FUNCTION_COVER_PERCENTAGE <= functions_covered
+
+print("Coverage is enough")
