0184413502ac51f0d3ee96c0fcf8bb85e38e8925
Ceyhun Şen <ceyhuusen@gmail.com>
2024-09-13 15:41:26 +0300

    config: Add new sections.

--- core/src/config.rs
+++ core/src/config.rs
@@ -16,75 +16,160 @@ use bitcoin::Network;
 use serde::{Deserialize, Serialize};
 use std::{fs::File, io::Read, path::PathBuf};
 
+/// PostgreSQL database configuration options.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Database {
+    /// PostgreSQL database host address.
+    pub host: String,
+    /// PostgreSQL database port.
+    pub port: usize,
+    /// PostgreSQL database user name.
+    pub user: String,
+    /// PostgreSQL database user password.
+    pub password: String,
+    /// PostgreSQL database name.
+    pub name: String,
+}
+impl Default for Database {
+    fn default() -> Self {
+        Self {
+            host: "127.0.0.1".to_string(),
+            port: 5432,
+            user: "clementine".to_string(),
+            password: "clementine".to_string(),
+            name: "clementine".to_string(),
+        }
+    }
+}
+
+/// Bitcoin connection configuration options.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Bitcoin {
+    /// Butcoin RPC url.
+    pub rpc_url: String,
+    /// Bitcoin RPC user.
+    pub rpc_user: String,
+    /// Bitcoin RPC user password.
+    pub rpc_password: String,
+    /// Bitcoin network to work on.
+    pub network: Network,
+}
+impl Default for Bitcoin {
+    fn default() -> Self {
+        Self {
+            network: Network::Regtest,
+            rpc_url: "http://127.0.0.1:18443".to_string(),
+            rpc_user: "admin".to_string(),
+            rpc_password: "admin".to_string(),
+        }
+    }
+}
+
+/// Operator configuration options.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Operator {
+    /// Number of operators.
+    pub count: usize,
+    /// Operators x-only public keys.
+    pub xonly_pks: Vec<secp256k1::XOnlyPublicKey>,
+    /// Number of blocks after which operator can take reimburse the bridge fund if they are honest.
+    pub takes_after: u32,
+    /// Operator: number of kickoff UTXOs per funding transaction.
+    pub kickoff_utxos_per_tx: usize,
+    /// All Secret keys. Just for testing purposes.
+    pub all_secret_keys: Option<Vec<secp256k1::SecretKey>>,
+}
+impl Default for Operator {
+    fn default() -> Self {
+        Self {
+            count: 3,
+            xonly_pks: vec![],
+            takes_after: 5,
+            kickoff_utxos_per_tx: 10,
+            all_secret_keys: None,
+        }
+    }
+}
+
+/// Verifier configuration options.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Verifier {
+    /// Number of verifiers.
+    pub count: usize,
+    /// Verifiers public keys.
+    pub public_keys: Vec<secp256k1::PublicKey>,
+    /// All Secret keys. Just for testing purposes.
+    pub all_secret_keys: Option<Vec<secp256k1::SecretKey>>,
+    /// Verifier endpoints.
+    pub endpoints: Option<Vec<String>>,
+}
+impl Default for Verifier {
+    fn default() -> Self {
+        Self {
+            public_keys: vec![],
+            count: 7,
+            all_secret_keys: None,
+            endpoints: None,
+        }
+    }
+}
+
 /// Configuration options for any Clementine target (tests, binaries etc.).
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BridgeConfig {
+    /// PostgreSQL database configuration options.
+    pub database: Database,
+    /// Bitcoin connection configuration options.
+    pub bitcoin: Bitcoin,
+    /// Operator configuration options.
+    pub operator: Operator,
+    /// Verifier configuration options.
+    pub verifier: Verifier,
+
     /// Host of the operator or the verifier
     pub host: String,
     /// Port of the operator or the verifier
     pub port: u16,
-    /// Bitcoin network to work on.
-    pub network: Network,
     /// Secret key for the operator or the verifier.
     pub secret_key: secp256k1::SecretKey,
-    /// Verifiers public keys.
-    pub verifiers_public_keys: Vec<secp256k1::PublicKey>,
-    /// Number of verifiers.
-    pub num_verifiers: usize,
-    /// Operators x-only public keys.
-    pub operators_xonly_pks: Vec<secp256k1::XOnlyPublicKey>,
     /// Operators wallet addresses.
     pub operator_wallet_addresses: Vec<bitcoin::Address<NetworkUnchecked>>,
     /// Number of operators.
-    pub num_operators: usize,
-    /// Operator's fee for withdrawal, in satoshis.
     pub operator_withdrawal_fee_sats: Option<u64>,
     /// Number of blocks after which user can take deposit back if deposit request fails.
     pub user_takes_after: u32,
-    /// Number of blocks after which operator can take reimburse the bridge fund if they are honest.
-    pub operator_takes_after: u32,
     /// Bridge amount in satoshis.
     pub bridge_amount_sats: u64,
-    /// Operator: number of kickoff UTXOs per funding transaction.
-    pub operator_num_kickoff_utxos_per_tx: usize,
     /// Threshold for confirmation.
     pub confirmation_threshold: u32,
-    /// Bitcoin remote procedure call URL.
-    pub bitcoin_rpc_url: String,
-    /// Bitcoin RPC user.
-    pub bitcoin_rpc_user: String,
-    /// Bitcoin RPC user password.
-    pub bitcoin_rpc_password: String,
-    /// All Secret keys. Just for testing purposes.
-    pub all_verifiers_secret_keys: Option<Vec<secp256k1::SecretKey>>,
-    /// All Secret keys. Just for testing purposes.
-    pub all_operators_secret_keys: Option<Vec<secp256k1::SecretKey>>,
-    /// Verifier endpoints.
-    pub verifier_endpoints: Option<Vec<String>>,
-    /// PostgreSQL database host address.
-    pub db_host: String,
-    /// PostgreSQL database port.
-    pub db_port: usize,
-    /// PostgreSQL database user name.
-    pub db_user: String,
-    /// PostgreSQL database user password.
-    pub db_password: String,
-    /// PostgreSQL database name.
-    pub db_name: String,
     /// Citrea RPC URL.
     pub citrea_rpc_url: String,
     /// Bridge contract address.
     pub bridge_contract_address: String,
 }
+impl Default for BridgeConfig {
+    fn default() -> Self {
+        Self {
+            database: Database::default(),
+            bitcoin: Bitcoin::default(),
+            operator: Operator::default(),
+            verifier: Verifier::default(),
 
-impl BridgeConfig {
-    /// Create a new `BridgeConfig` with default values.
-    pub fn new() -> Self {
-        BridgeConfig {
-            ..Default::default()
+            host: "127.0.0.1".to_string(),
+            port: 3030,
+            secret_key: secp256k1::SecretKey::new(&mut secp256k1::rand::thread_rng()),
+            user_takes_after: 5,
+            bridge_amount_sats: 100_000_000,
+            confirmation_threshold: 1,
+            operator_withdrawal_fee_sats: None,
+            operator_wallet_addresses: vec![],
+            citrea_rpc_url: "http://127.0.0.1:12345".to_string(),
+            bridge_contract_address: "3100000000000000000000000000000000000002".to_string(),
         }
     }
+}
 
+impl BridgeConfig {
     /// Read contents of a TOML file and generate a `BridgeConfig`.
     pub fn try_parse_file(path: PathBuf) -> Result<Self, BridgeError> {
         let mut contents = String::new();
@@ -113,41 +198,6 @@ impl BridgeConfig {
     }
 }
 
-impl Default for BridgeConfig {
-    fn default() -> Self {
-        Self {
-            host: "127.0.0.1".to_string(),
-            port: 3030,
-            secret_key: secp256k1::SecretKey::new(&mut secp256k1::rand::thread_rng()),
-            verifiers_public_keys: vec![],
-            num_verifiers: 7,
-            operators_xonly_pks: vec![],
-            operator_wallet_addresses: vec![],
-            num_operators: 3,
-            operator_withdrawal_fee_sats: None,
-            user_takes_after: 5,
-            operator_takes_after: 5,
-            bridge_amount_sats: 100_000_000,
-            operator_num_kickoff_utxos_per_tx: 10,
-            confirmation_threshold: 1,
-            network: Network::Regtest,
-            bitcoin_rpc_url: "http://127.0.0.1:18443".to_string(),
-            bitcoin_rpc_user: "admin".to_string(),
-            bitcoin_rpc_password: "admin".to_string(),
-            all_verifiers_secret_keys: None,
-            all_operators_secret_keys: None,
-            verifier_endpoints: None,
-            db_host: "127.0.0.1".to_string(),
-            db_port: 5432,
-            db_user: "postgres".to_string(),
-            db_password: "postgres".to_string(),
-            db_name: "postgres".to_string(),
-            citrea_rpc_url: "http://127.0.0.1:12345".to_string(),
-            bridge_contract_address: "3100000000000000000000000000000000000002".to_string(),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::BridgeConfig;
@@ -170,7 +220,7 @@ mod tests {
             Err(e) => println!("{e:#?}"),
         };
 
-        let init = BridgeConfig::new();
+        let init = BridgeConfig::default();
         match BridgeConfig::try_parse_from(toml::to_string(&init).unwrap()) {
             Ok(c) => println!("{c:#?}"),
             Err(e) => panic!("{e:#?}"),
