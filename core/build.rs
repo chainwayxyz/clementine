57f14099a7524d5d1f52d9cfdf16bcf7742bd46b
Ceyhun Şen <ceyhuusen@gmail.com>
2024-10-14 15:19:21 +0300

    build: Add protobuf compiler to build.rs.

--- core/build.rs
+++ core/build.rs
@@ -1,4 +1,25 @@
+use std::path::Path;
+
+fn compile_protobuf() {
+    let proto_root = Path::new(env!("CARGO_MANIFEST_DIR")).join("../proto");
+    let protos = &["clementine.proto"];
+
+    let proto_files: Vec<String> = protos
+        .iter()
+        .map(|proto| proto_root.join(proto).to_str().unwrap().to_owned())
+        .collect();
+
+    // Compile server and client code from proto files
+    if let Err(e) = tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir("./src/")
+        .compile_protos(&proto_files, &[proto_root.to_str().unwrap()]) 
+    {
+        panic!("Failed to compile protos: {}", e);
+    }
+}
+
 fn main() {
-    tonic_build::compile_protos("../proto/clementine.proto")
-        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+    compile_protobuf();
 }
