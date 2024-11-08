aae3995d28d39b412c3f3a8dd825013cb508a2c9
Ceyhun Şen <ceyhuusen@gmail.com>
2024-11-08 17:00:47 +0700

    database: Add get_non_proven_block.

--- core/src/database/chain_proof.rs
+++ core/src/database/chain_proof.rs
@@ -5,7 +5,11 @@ use super::{
     Database,
 };
 use crate::errors::BridgeError;
-use bitcoin::{block, hashes::Hash, BlockHash};
+use bitcoin::{
+    block::{self, Header},
+    hashes::Hash,
+    BlockHash,
+};
 use risc0_zkvm::Receipt;
 use sqlx::Postgres;
 
@@ -114,6 +118,35 @@ impl Database {
             (Some(height), Some(hash)) => Ok((height as u64, hash.0)),
             _ => Ok((0, BlockHash::all_zeros())),
         }
+    }
+
+    /// TODO: Return Option::None in case of last element is proven but it's
+    /// ancestor is not.
+    #[tracing::instrument(skip(self), err(level = tracing::Level::ERROR), ret(level = tracing::Level::TRACE))]
+    pub async fn get_non_proven_block(
+        &self,
+        tx: Option<&mut sqlx::Transaction<'_, Postgres>>,
+    ) -> Result<(BlockHash, Header, i32, Receipt), BridgeError> {
+        let query = sqlx::query_as(
+            "SELECT h1.block_hash,
+                h1.block_header,
+                h1.height,
+                h2.proof
+            FROM header_chain_proofs h1
+            JOIN header_chain_proofs h2 ON h1.height = h2.height + 1
+            WHERE h2.proof IS NOT NULL
+            ORDER BY h1.height
+            LIMIT 1;",
+        );
+
+        let result: (BlockHashDB, BlockHeaderDB, i32, Vec<u8>) = match tx {
+            Some(tx) => query.fetch_one(&mut **tx).await,
+            None => query.fetch_one(&self.connection).await,
+        }?;
+
+        let receipt: Receipt = borsh::from_slice(&result.3)?;
+
+        Ok((result.0 .0, result.1 .0, result.2, receipt))
     }
 }
 
@@ -251,5 +284,80 @@ mod tests {
         let read_receipt = db.get_block_proof_by_hash(None, block_hash).await.unwrap();
         assert_eq!(receipt.journal, read_receipt.journal);
         assert_eq!(receipt.metadata, read_receipt.metadata);
+    }
+
+    #[tokio::test]
+    pub async fn get_non_proven_block() {
+        let config = create_test_config_with_thread_name("test_config.toml", None).await;
+        let db = Database::new(&config).await.unwrap();
+
+        assert!(db.get_non_proven_block(None).await.is_err());
+
+        let base_height = 0x45;
+
+        // Save dummy block without a proof.
+        let block = block::Block {
+            header: Header {
+                version: Version::TWO,
+                prev_blockhash: BlockHash::all_zeros(),
+                merkle_root: TxMerkleNode::all_zeros(),
+                time: 0x1F,
+                bits: CompactTarget::default(),
+                nonce: 0x45,
+            },
+            txdata: vec![],
+        };
+        let block_hash = block.block_hash();
+        let height = base_height;
+        db.save_new_block(None, block_hash, block.header, height)
+            .await
+            .unwrap();
+        assert!(db.get_non_proven_block(None).await.is_err());
+
+        // Save dummy block with a proof.
+        let block = block::Block {
+            header: Header {
+                version: Version::TWO,
+                prev_blockhash: BlockHash::all_zeros(),
+                merkle_root: TxMerkleNode::all_zeros(),
+                time: 0x1F,
+                bits: CompactTarget::default(),
+                nonce: 0x45 + 1,
+            },
+            txdata: vec![],
+        };
+        let block_hash1 = block.block_hash();
+        let height1 = base_height + 1;
+        db.save_new_block(None, block_hash1, block.header, height1)
+            .await
+            .unwrap();
+        let receipt =
+            Receipt::try_from_slice(include_bytes!("../../tests/data/first_1.bin")).unwrap();
+        db.save_block_proof(None, block_hash1, receipt.clone())
+            .await
+            .unwrap();
+        assert!(db.get_non_proven_block(None).await.is_err());
+
+        // Save dummy block without a proof.
+        let block = block::Block {
+            header: Header {
+                version: Version::TWO,
+                prev_blockhash: BlockHash::all_zeros(),
+                merkle_root: TxMerkleNode::all_zeros(),
+                time: 0x1F,
+                bits: CompactTarget::default(),
+                nonce: 0x45 + 3,
+            },
+            txdata: vec![],
+        };
+        let block_hash2 = block.block_hash();
+        let height2 = base_height + 2;
+        db.save_new_block(None, block_hash2, block.header, height2)
+            .await
+            .unwrap();
+
+        let res = db.get_non_proven_block(None).await.unwrap();
+        assert_eq!(res.0, block_hash2);
+        assert_eq!(res.2 as u64, height2);
     }
 }
