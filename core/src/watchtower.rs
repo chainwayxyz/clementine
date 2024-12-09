d2c4bc012e3b1400ae2124f920e630def8eb0933
Ceyhun Şen <ceyhuusen@gmail.com>
2024-12-09 10:53:39 +0300

    watchtower: Add initial struct.

--- core/src/watchtower.rs
+++ core/src/watchtower.rs
@@ -1,7 +1,60 @@
-use crate::{database::Database, extended_rpc::ExtendedRpc};
+use crate::{
+    config::BridgeConfig,
+    database::Database,
+    errors::BridgeError,
+    extended_rpc::ExtendedRpc,
+    rpc::{
+        self,
+        clementine::{
+            clementine_operator_client::ClementineOperatorClient,
+            clementine_verifier_client::ClementineVerifierClient,
+        },
+    },
+};
 
 #[derive(Debug, Clone)]
 pub struct Watchtower {
-    _rpc: ExtendedRpc,
-    _db: Database,
+    rpc: ExtendedRpc,
+    db: Database,
+    pub(crate) verifier_clients: Vec<ClementineVerifierClient<tonic::transport::Channel>>,
+    pub(crate) operator_clients: Vec<ClementineOperatorClient<tonic::transport::Channel>>,
+}
+
+impl Watchtower {
+    pub async fn new(config: BridgeConfig) -> Result<Self, BridgeError> {
+        let rpc = ExtendedRpc::new(
+            config.bitcoin_rpc_url.clone(),
+            config.bitcoin_rpc_user.clone(),
+            config.bitcoin_rpc_password.clone(),
+        )
+        .await;
+        let db = Database::new(&config).await?;
+
+        let verifier_endpoints =
+            config
+                .verifier_endpoints
+                .clone()
+                .ok_or(BridgeError::ConfigError(
+                    "Couldn't find operator endpoints in config file!".to_string(),
+                ))?;
+        let verifier_clients =
+            rpc::get_clients(verifier_endpoints, ClementineVerifierClient::connect).await?;
+
+        let operator_endpoints =
+            config
+                .operator_endpoints
+                .clone()
+                .ok_or(BridgeError::ConfigError(
+                    "Couldn't find operator endpoints in config file!".to_string(),
+                ))?;
+        let operator_clients =
+            rpc::get_clients(operator_endpoints, ClementineOperatorClient::connect).await?;
+
+        Ok(Self {
+            rpc,
+            db,
+            verifier_clients,
+            operator_clients,
+        })
+    }
 }
