736373e6e5da4b2c26338f7b83056a0fd2209a34
Ceyhun Şen <ceyhuusen@gmail.com>
2025-02-19 11:25:55 +0300

    tests: Reapply withdrawal.

--- core/tests/withdrawal.rs
+++ core/tests/withdrawal.rs
@@ -1,12 +1,17 @@
 use async_trait::async_trait;
 use bitcoincore_rpc::RpcApi;
 use citrea_e2e::{
-    bitcoin::DEFAULT_FINALITY_DEPTH,
-    config::{TestCaseConfig, TestCaseDockerConfig},
+    bitcoin::{BitcoinNode, DEFAULT_FINALITY_DEPTH},
+    config::{BitcoinConfig, TestCaseConfig, TestCaseDockerConfig},
     framework::TestFramework,
     test_case::{TestCase, TestCaseRunner},
     Result,
 };
+use clementine_core::extended_rpc::ExtendedRpc;
+use clementine_core::{config::BridgeConfig, database::Database, utils::initialize_logger};
+use std::sync::Arc;
+
+mod common;
 
 struct DockerIntegrationTest;
 #[async_trait]
@@ -25,10 +30,31 @@ impl TestCase for DockerIntegrationTest {
     }
 
     async fn run_test(&mut self, f: &mut TestFramework) -> Result<()> {
-        let sequencer = f.sequencer.as_ref().unwrap();
-        let batch_prover = f.batch_prover.as_ref().unwrap();
-        let full_node = f.full_node.as_ref().unwrap();
-        let da = f.bitcoin_nodes.get(0).unwrap();
+        let mut config = create_test_config_with_thread_name!(None);
+        let regtest = create_regtest_rpc!(config);
+        let _rpc = regtest.rpc().clone();
+
+        let sequencer = f.sequencer.as_ref().expect("Sequencer is present");
+        let batch_prover = f.batch_prover.as_ref().expect("Batch prover is present");
+        let full_node = f.full_node.as_ref().expect("Full node is present");
+
+        let port: u16 = config
+            .bitcoin_rpc_url
+            .split("http://127.0.0.1:")
+            .next()
+            .expect("URL is present")
+            .split("/")
+            .next()
+            .expect("Port is present")
+            .parse()
+            .expect("Port is a number");
+        let config = BitcoinConfig {
+            rpc_user: config.bitcoin_rpc_user,
+            rpc_password: config.bitcoin_rpc_password,
+            rpc_port: port,
+            ..Default::default()
+        };
+        let da = BitcoinNode::new(&config, Arc::new(None)).await?;
 
         let min_soft_confirmations_per_commitment =
             Self::sequencer_config().min_soft_confirmations_per_commitment;
