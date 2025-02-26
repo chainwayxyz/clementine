78eb8da80c1dcb9197261c6a5f6f49106e77b8bc
aoengin <ahmetoguzengin@hotmail.com>
2025-02-26 12:30:54 +0300

    refactor: remove unused files

--- circuits-lib/src/operator/mod.rs
+++
@@ -1,8 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Debug, BorshDeserialize, BorshSerialize)]
-pub struct OperatorCircuitInput {}
-
-#[derive(Serialize, Deserialize, Eq, PartialEq, Clone, Debug, BorshDeserialize, BorshSerialize)]
-pub struct OperatorCircuitOutput {}
