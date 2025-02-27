f2a8e2099f4f108278bad4abf82791b66077ca30
Ceyhun Şen <ceyhuusen@gmail.com>
2025-02-27 21:08:51 +0300

    chore(test/citrea): Move citrea_url config change to update_config_with_citrea_e2e_da and remove deposit code in citrea_withdraw test.

--- core/src/test/withdraw.rs
+++ core/src/test/withdraw.rs
@@ -3,7 +3,6 @@ use crate::{
     test::common::{
         citrea::{self, EVM_ADDRESSES, SECRET_KEYS},
         create_test_config_with_thread_name, generate_withdrawal_transaction_and_signature,
-        run_single_deposit,
     },
     utils::SECP,
     EVMAddress,
@@ -15,31 +14,21 @@ use alloy::{
     providers::Provider,
     rpc::types::TransactionRequest,
     signers::local::PrivateKeySigner,
-    sol,
 };
 use alloy::{providers::ProviderBuilder, transports::http::reqwest::Url};
 use async_trait::async_trait;
 use bitcoin::{secp256k1::SecretKey, Address, Amount};
-use bitcoincore_rpc::RpcApi;
 use citrea_e2e::{
     config::{BitcoinConfig, SequencerConfig, TestCaseConfig, TestCaseDockerConfig},
     framework::TestFramework,
     test_case::{TestCase, TestCaseRunner},
     Result,
 };
-use std::{str::FromStr, thread::sleep, time::Duration};
+use std::str::FromStr;
 
-// Codegen from ABI file to interact with the contract.
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    IWETH9,
-    "src/test/common/citrea/Bridge.json"
-);
-
-struct CitreaDepositAndWithdraw;
+struct CitreaWithdraw;
 #[async_trait]
-impl TestCase for CitreaDepositAndWithdraw {
+impl TestCase for CitreaWithdraw {
     fn bitcoin_config() -> BitcoinConfig {
         BitcoinConfig {
             extra_args: vec![
@@ -66,7 +55,6 @@ impl TestCase for CitreaDepositAndWithdraw {
 
     fn sequencer_config() -> SequencerConfig {
         SequencerConfig {
-            // min_soft_confirmations_per_commitment: 50,
             test_mode: false,
             bridge_initialize_params: "000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000000000000000002d4a20423a0b35060e62053765e2aba342f1c242e78d68f5248aca26e703c0c84ca322ac0063066369747265611400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a08000000003b9aca006800000000000000000000000000000000000000000000".to_string(),
             ..Default::default()
@@ -79,7 +67,7 @@ impl TestCase for CitreaDepositAndWithdraw {
             .unwrap();
 
         let mut config = create_test_config_with_thread_name(None).await;
-        citrea::update_config_with_citrea_e2e_da(&mut config, da);
+        citrea::update_config_with_citrea_e2e_values(&mut config, da, sequencer);
 
         let rpc = ExtendedRpc::connect(
             config.bitcoin_rpc_url.clone(),
@@ -88,54 +76,17 @@ impl TestCase for CitreaDepositAndWithdraw {
         )
         .await?;
 
-        let citrea_url = format!(
-            "http://{}:{}",
-            sequencer.config.rollup.rpc.bind_host, sequencer.config.rollup.rpc.bind_port
-        );
-        config.citrea_rpc_url = citrea_url.clone();
-
         // EVM_ADDRESSES[0]
         let evm_address = EVMAddress([
             0xf3, 0x9F, 0xd6, 0xe5, 0x1a, 0xad, 0x88, 0xF6, 0xF4, 0xce, 0x6a, 0xB8, 0x82, 0x72,
             0x79, 0xcf, 0xff, 0xb9, 0x22, 0x66,
         ]);
-        let (_verifiers, _operators, _aggregator, _watchtowers, _deposit_outpoint, move_txid) =
-            run_single_deposit(&mut config, rpc.clone(), Some(evm_address)).await?;
-        rpc.mine_blocks(1).await.unwrap();
 
-        let tx = rpc.client.get_raw_transaction(&move_txid, None).await?;
-        let tx_info = rpc
-            .client
-            .get_raw_transaction_info(&move_txid, None)
-            .await?;
-        let block = rpc
-            .client
-            .get_block(&tx_info.blockhash.expect("Not None"))
-            .await?;
-        rpc.mine_blocks(101).await.unwrap();
-        let block_height = rpc.client.get_block_info(&block.block_hash()).await?.height;
-
-        while citrea::block_number(sequencer.client.http_client().clone()).await?
-            < block_height.try_into().unwrap()
-        {
-            tracing::debug!("Waiting for block to be mined");
-            rpc.mine_blocks(1).await.unwrap();
-            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-        }
-
-        citrea::deposit(
-            sequencer.client.http_client().clone(),
-            block,
-            block_height.try_into().expect("Will not fail"),
-            tx,
-        )
-        .await?;
-
-        sleep(Duration::from_secs(3));
         let balance = citrea::eth_get_balance(sequencer.client.http_client().clone(), evm_address)
             .await
             .unwrap();
-        // Has initial funds. Therefore should be greater than the bridge amount.
+        // Wallet has initial funds and should be greater than the bridge amount
+        // to start test.
         assert!(
             balance / 10_000_000_000 >= (config.protocol_paramset().bridge_amount.to_sat()).into()
         );
@@ -169,7 +120,7 @@ impl TestCase for CitreaDepositAndWithdraw {
             .with_chain_id(Some(chain_id));
         let provider = ProviderBuilder::new()
             .wallet(EthereumWallet::from(key))
-            .on_http(Url::parse(&citrea_url).unwrap());
+            .on_http(Url::parse(&config.citrea_rpc_url).unwrap());
 
         let alice = primitives::Address::from_str(EVM_ADDRESSES[0]).unwrap();
         let bob = primitives::Address::from_str(EVM_ADDRESSES[1]).unwrap();
@@ -198,6 +149,6 @@ impl TestCase for CitreaDepositAndWithdraw {
 }
 
 #[tokio::test]
-async fn citrea_deposit_and_withdraw() -> Result<()> {
-    TestCaseRunner::new(CitreaDepositAndWithdraw).run().await
+async fn citrea_withdraw() -> Result<()> {
+    TestCaseRunner::new(CitreaWithdraw).run().await
 }
