cf3a4cf837fa4a385de0e63f66b93ba752837ae6
Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>
2025-03-04 14:55:45 +0300

    bridge circuit implementation (#575)
    * add bridge circuit.
    
    Co-authored-by: ozankaymak <ozan.kaymak@boun.edu.tr>
    
    * refactor: remove unused files
    
    * refactor: update dependencies
    
    * refactor: reorganize circuits library
    
    * fix: update L1 height in light client proof fetching
    
    * refactor: clean up code formatting and update dependencies in Cargo.toml files
    
    * refactor: reorganize circuits library structure
    
    * fix: downgrade ark-groth16 version to 0.4.0
    
    * refactor: update dependencies and clean up Cargo.toml files
    
    * refactor: update dependencies and improve bridge circuit implementation
    
    * refactor: clean up Cargo.toml files and update dependencies for workspace structure to isolate bridge circuit
    
    * refactor: reorganize module structure and update dependencies in bridge circuit implementation
    
    * refactor: update workspace structure and dependencies
    
    * refactor: update Dockerfile paths and clean up build scripts in bridge circuit
    
    * refactor: clean up build scripts and remove unused example in bridge circuit
    
    * fmt
    
    * WIP
    
    * refactor: remove obsolete output file from bridge circuit
    
    * refactor: update dependencies to use risc0-to-bitvm2-core and bump risc0-build version
    
    * refactor: update dependencies to version 1.2.5 and adjust source URLs
    
    * refactor: update binary files and adjust payout transaction index
    
    * refactor: update bridge circuit and host to generate a proof
    
    * fmt
    
    * fmt
    
    * refactor: create a separate config file for parameters
    
    * refactor: remove duplicate code
    
    * fmt
    
    * refactor: update fetch_light_client_proof and fetch_storage_proof to accept provider as parameter
    
    * refactor: proper naming for input and outputs
    
    * refactor: improve code by removing multiple "copy_from_slice" calls
    
    * refactor: restructure main function and introduce BridgeCircuitHostParams for better input management
    
    * refactor: simplify concatenation of move_tx_id and deposit_mapping_storage_index
    
    * clippy
    
    * fmt
    
    * ci: add Risc0 installation step to code checks workflow
    
    * merge dev
    
    * ci: add Risc0 installation step to code checks workflow
    
    * chore: remove unused packages
    
    * refactor: revert some changes back
    
    ---------
    
    Co-authored-by: ozankaymak <ozan.kaymak@boun.edu.tr>

---
+++ build-files/bridge_circuit_build.dockerfile
@@ -0,0 +1,32 @@
+FROM risczero/risc0-guest-builder:r0.1.81.0 AS build
+
+WORKDIR /src
+
+# Copy the entire project structure
+COPY risc0-circuits/bridge-circuit risc0-circuits/bridge-circuit
+
+# Might be heavy in the future, but for now it's fine
+COPY circuits-lib circuits-lib
+
+COPY Cargo.toml Cargo.toml
+
+# Set compile-time environment variables
+ENV CARGO_MANIFEST_PATH="risc0-circuits/bridge-circuit/guest/Cargo.toml"
+ENV RUSTFLAGS="-C passes=loweratomic -C link-arg=-Ttext=0x00200800 -C link-arg=--fatal-warnings"
+ENV CARGO_TARGET_DIR="risc0-circuits/bridge-circuit/guest/target"
+ENV CC_riscv32im_risc0_zkvm_elf="/root/.local/share/cargo-risczero/cpp/bin/riscv32-unknown-elf-gcc"
+ENV CFLAGS_riscv32im_risc0_zkvm_elf="-march=rv32im -nostdlib"
+
+# Set network environment variable
+ARG BITCOIN_NETWORK=testnet4
+ENV BITCOIN_NETWORK=${BITCOIN_NETWORK}
+
+# Only run the build once with the environment variable set
+RUN echo "Building for network: ${BITCOIN_NETWORK}" && \
+    cargo +risc0 fetch --target riscv32im-risc0-zkvm-elf --manifest-path ${CARGO_MANIFEST_PATH} && \
+    cargo +risc0 build --release --target riscv32im-risc0-zkvm-elf --manifest-path ${CARGO_MANIFEST_PATH}
+
+FROM scratch AS export
+ARG BITCOIN_NETWORK
+COPY --from=build /src/risc0-circuits/bridge-circuit/guest/target/riscv32im-risc0-zkvm-elf/release ../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/bridge-circuit-guest
+COPY --from=build /src/risc0-circuits/bridge-circuit/guest/target/riscv32im-risc0-zkvm-elf/release/bridge-circuit-guest risc0-circuits/elfs/${BITCOIN_NETWORK}-bridge-circuit-guest
---
+++ circuits-lib/src/bridge_circuit/storage_proof.rs
@@ -0,0 +1,91 @@
+use crate::bridge_circuit_core::structs::StorageProof;
+use alloy_primitives::Bytes;
+use alloy_primitives::{Keccak256, U256};
+use alloy_rpc_types::EIP1186StorageProof;
+use jmt::{proof::SparseMerkleProof, KeyHash};
+
+const ADDRESS: [u8; 20] = hex_literal::hex!("3100000000000000000000000000000000000002");
+
+// STORAGRE SLOTES of DATA STRUCTURES ON BRIDGE CONTRACT
+const UTXOS_STORAGE_INDEX: [u8; 32] =
+    hex_literal::hex!("0000000000000000000000000000000000000000000000000000000000000026");
+const DEPOSIT_MAPPING_STORAGE_INDEX: [u8; 32] =
+    hex_literal::hex!("0000000000000000000000000000000000000000000000000000000000000027");
+
+pub fn verify_storage_proofs(storage_proof: &StorageProof, state_root: [u8; 32]) -> String {
+    let utxo_storage_proof: EIP1186StorageProof =
+        serde_json::from_str(&storage_proof.storage_proof_utxo).unwrap();
+    let deposit_storage_proof: EIP1186StorageProof =
+        serde_json::from_str(&storage_proof.storage_proof_deposit_idx).unwrap();
+
+    println!(
+        "deposit storage proof value {:?}",
+        deposit_storage_proof.value
+    );
+
+    let mut keccak = Keccak256::new();
+    keccak.update(UTXOS_STORAGE_INDEX);
+    let hash = keccak.finalize();
+
+    let storage_address: U256 =
+        U256::from_be_bytes(<[u8; 32]>::try_from(&hash[..]).expect("Slice with incorrect length"));
+    let storage_key: alloy_primitives::Uint<256, 4> =
+        storage_address + U256::from(storage_proof.index * 2);
+
+    let mut concantenated: [u8; 64] = [0; 64];
+    concantenated[..32].copy_from_slice(&storage_proof.txid_hex);
+    concantenated[32..].copy_from_slice(&DEPOSIT_MAPPING_STORAGE_INDEX);
+
+    let mut keccak = Keccak256::new();
+    keccak.update(concantenated);
+    let mut hash = keccak.finalize().0;
+    hash.reverse(); // To match endianess
+
+    if hash != deposit_storage_proof.key.as_b256().0 {
+        panic!("Invalid deposit storage key.");
+    }
+
+    if storage_key.to_le_bytes() != utxo_storage_proof.key.as_b256().0
+        || U256::from(storage_proof.index) != deposit_storage_proof.value
+    {
+        panic!("Invalid withdrawal UTXO storage key.");
+    }
+
+    storage_verify(&deposit_storage_proof, state_root);
+    println!("Deposit storage proof verification successful!");
+
+    storage_verify(&utxo_storage_proof, state_root);
+    println!("UTXO storage proof verification successful!");
+
+    utxo_storage_proof.value.to_string()
+}
+
+fn storage_verify(storage_proof: &EIP1186StorageProof, expected_root_hash: [u8; 32]) {
+    println!("key {:?}", storage_proof.key.as_b256().0);
+    let storage_key = [
+        b"Evm/s/",
+        ADDRESS.as_slice(),
+        &[32],
+        U256::from_le_slice(&storage_proof.key.as_b256().0)
+            .to_be_bytes::<32>()
+            .as_slice(),
+    ]
+    .concat();
+    let key_hash = KeyHash::with::<sha2::Sha256>(storage_key.clone());
+
+    let proved_value = if storage_proof.proof[1] == Bytes::from("y") {
+        // Storage value exists and it's serialized form is:
+        let bytes = [&[32], storage_proof.value.to_be_bytes::<32>().as_slice()].concat();
+        Some(bytes)
+    } else {
+        // Storage value does not exist
+        None
+    };
+
+    let storage_proof: SparseMerkleProof<sha2::Sha256> =
+        borsh::from_slice(&storage_proof.proof[0]).unwrap();
+
+    storage_proof
+        .verify(jmt::RootHash(expected_root_hash), key_hash, proved_value)
+        .expect("Account storage proof must be valid");
+}
