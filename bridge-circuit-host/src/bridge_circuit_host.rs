909678ec995e1721e562ee1a26d4e37d181d06ca
aoengin <ahmetoguzengin@hotmail.com>
2025-03-05 15:49:57 +0300

    refactor: update bridge circuit host parameters

--- bridge-circuit-host/src/bridge_circuit_host.rs
+++ bridge-circuit-host/src/bridge_circuit_host.rs
@@ -1,87 +1,30 @@
 use crate::structs::BridgeCircuitHostParams;
 use crate::{fetch_light_client_proof, fetch_storage_proof};
+use alloy_rpc_client::RpcClient;
 use borsh;
-use circuits_lib::bridge_circuit_core::groth16::CircuitGroth16Proof;
 use circuits_lib::bridge_circuit_core::structs::{BridgeCircuitInput, WorkOnlyCircuitInput};
-use circuits_lib::bridge_circuit_core::winternitz::{
-    generate_public_key, sign_digits, Parameters, WinternitzHandler,
-};
-use rand::{rngs::SmallRng, Rng, SeedableRng};
+
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts, Receipt};
-use std::convert::TryInto;
-use std::fs;
 
 const BRIDGE_CIRCUIT_ELF: &[u8] =
     include_bytes!("../../risc0-circuits/elfs/testnet4-bridge-circuit-guest");
 const WORK_ONLY_ELF: &[u8] = include_bytes!("../../risc0-circuits/elfs/testnet4-work-only-guest");
 
-pub fn prove_work_only(receipt: Receipt, input: &WorkOnlyCircuitInput) -> Receipt {
-    let mut binding = ExecutorEnv::builder();
-    binding.add_assumption(receipt);
-    let env = binding.write_slice(&borsh::to_vec(&input).unwrap());
-    let env = env.build().unwrap();
-    let prover = default_prover();
-    prover
-        .prove_with_opts(env, WORK_ONLY_ELF, &ProverOpts::groth16())
-        .unwrap()
-        .receipt
-}
-
-pub async fn prove_bridge_circuit(bridge_circuit_host_params: BridgeCircuitHostParams) {
-    let g16_proof_receipt: &risc0_zkvm::Groth16Receipt<risc0_zkvm::ReceiptClaim> =
-        bridge_circuit_host_params
-            .work_only_groth16_proof_receipt
-            .inner
-            .groth16()
-            .unwrap();
-    println!("G16 PROOF RECEIPT: {:?}", g16_proof_receipt);
-
-    let seal =
-        CircuitGroth16Proof::from_seal(g16_proof_receipt.seal.as_slice().try_into().unwrap());
-
-    let compressed_proof = seal.to_compressed().unwrap();
-
-    let commited_total_work: [u8; 16] = bridge_circuit_host_params
-        .work_only_groth16_proof_receipt
-        .journal
-        .bytes
-        .try_into()
-        .unwrap();
-
-    let mut compressed_proof_and_total_work: Vec<u8> = vec![0; 144];
-    compressed_proof_and_total_work[0..128].copy_from_slice(&compressed_proof);
-    compressed_proof_and_total_work[128..144].copy_from_slice(&commited_total_work);
-
-    let n0 = compressed_proof_and_total_work.len();
-    let log_d = 8;
-    let params = Parameters::new(n0.try_into().unwrap(), log_d);
-    let input: u64 = 1;
-    let mut rng = SmallRng::seed_from_u64(input);
-    let secret_key: Vec<u8> = (0..n0).map(|_| rng.gen()).collect();
-    let pub_key: Vec<[u8; 20]> = generate_public_key(&params, &secret_key);
-    let signature = sign_digits(&params, &secret_key, &compressed_proof_and_total_work);
-
+pub async fn prove_bridge_circuit(
+    bridge_circuit_host_params: BridgeCircuitHostParams,
+    light_client_rpc_client: RpcClient,
+    citrea_rpc_client: RpcClient,
+) -> Receipt {
     let (light_client_proof, _lcp_receipt) =
-        fetch_light_client_proof(72471, bridge_circuit_host_params.light_client_rpc_client)
+        fetch_light_client_proof(72471, light_client_rpc_client)
             .await
             .unwrap();
 
     // Check if L2 height is correct ??
-    let storage_proof = fetch_storage_proof(
-        &light_client_proof.l2_height,
-        bridge_circuit_host_params.citrea_rpc_client,
-    )
-    .await;
-
-    let winternitz_details = WinternitzHandler {
-        pub_key,
-        params,
-        signature: Some(signature),
-        message: Some(compressed_proof_and_total_work),
-    };
+    let storage_proof = fetch_storage_proof(&light_client_proof.l2_height, citrea_rpc_client).await;
 
     let bridge_circuit_input: BridgeCircuitInput = BridgeCircuitInput {
-        winternitz_details: vec![winternitz_details],
+        winternitz_details: bridge_circuit_host_params.winternitz_details,
         hcp: bridge_circuit_host_params.block_header_circuit_output, // This will change in the future
         payout_spv: bridge_circuit_host_params.spv,
         lcp: light_client_proof,
@@ -96,14 +39,23 @@ pub async fn prove_bridge_circuit(bridge_circuit_host_params: BridgeCircuitHostP
     let env = env.build().unwrap();
     let prover = default_prover();
 
-    println!("PROVING WINTERNITZ CIRCUIT");
-    let receipt = prover
+    println!("PROVING Bridge CIRCUIT");
+    prover
         .prove_with_opts(env, BRIDGE_CIRCUIT_ELF, &ProverOpts::succinct())
         .unwrap()
-        .receipt;
-    println!("RECEIPT: {:?}", receipt);
-    let receipt_bytes = borsh::to_vec(&receipt).unwrap();
-    fs::write("proof.bin", &receipt_bytes).expect("Failed to write receipt to output file");
+        .receipt
+}
+
+pub fn prove_work_only(receipt: Receipt, input: &WorkOnlyCircuitInput) -> Receipt {
+    let mut binding = ExecutorEnv::builder();
+    binding.add_assumption(receipt);
+    let env = binding.write_slice(&borsh::to_vec(&input).unwrap());
+    let env = env.build().unwrap();
+    let prover = default_prover();
+    prover
+        .prove_with_opts(env, WORK_ONLY_ELF, &ProverOpts::groth16())
+        .unwrap()
+        .receipt
 }
 
 #[cfg(test)]
@@ -114,13 +66,19 @@ mod tests {
     use bitcoin::hashes::Hash;
     use bitcoin::Transaction;
     use borsh::BorshDeserialize;
+    use circuits_lib::bridge_circuit_core::groth16::CircuitGroth16Proof;
     use circuits_lib::bridge_circuit_core::structs::WorkOnlyCircuitInput;
+    use circuits_lib::bridge_circuit_core::winternitz::{
+        generate_public_key, sign_digits, Parameters, WinternitzHandler,
+    };
+    use rand::{rngs::SmallRng, Rng, SeedableRng};
     use risc0_to_bitvm2_core::header_chain::BlockHeaderCircuitOutput;
     use risc0_to_bitvm2_core::header_chain::CircuitBlockHeader;
     use risc0_to_bitvm2_core::merkle_tree::BitcoinMerkleTree;
     use risc0_to_bitvm2_core::mmr_native::MMRNative;
     use risc0_to_bitvm2_core::spv::SPV;
     use risc0_zkvm::Receipt;
+    use std::convert::TryInto;
 
     use super::*;
 
@@ -163,6 +121,33 @@ mod tests {
         }
     }
 
+    fn generate_winternitz(
+        compressed_proof: &[u8; 128],
+        committed_total_work: &[u8],
+    ) -> WinternitzHandler {
+        let compressed_proof_and_total_work: Vec<u8> =
+            [compressed_proof, committed_total_work].concat();
+        println!(
+            "Compressed proof and total work: {:?}",
+            compressed_proof_and_total_work
+        );
+        let n0 = compressed_proof_and_total_work.len();
+        let log_d = 8;
+        let params = Parameters::new(n0.try_into().unwrap(), log_d);
+        let input: u64 = 1;
+        let mut rng = SmallRng::seed_from_u64(input);
+        let secret_key: Vec<u8> = (0..n0).map(|_| rng.gen()).collect();
+        let pub_key: Vec<[u8; 20]> = generate_public_key(&params, &secret_key);
+        let signature = sign_digits(&params, &secret_key, &compressed_proof_and_total_work);
+
+        WinternitzHandler {
+            pub_key,
+            params,
+            signature: Some(signature),
+            message: Some(compressed_proof_and_total_work),
+        }
+    }
+
     #[tokio::test]
     #[ignore = "This test will take an eternity. Only for debugging purposes."]
     async fn bridge_circuit_test() {
@@ -178,18 +163,39 @@ mod tests {
         let work_only_circuit_input: WorkOnlyCircuitInput = WorkOnlyCircuitInput {
             header_chain_circuit_output: block_header_circuit_output.clone(),
         };
+
         println!("PROVING WORK ONLY CIRCUIT");
         let work_only_groth16_proof_receipt: Receipt =
             prove_work_only(headerchain_proof, &work_only_circuit_input);
 
+        let g16_proof_receipt: &risc0_zkvm::Groth16Receipt<risc0_zkvm::ReceiptClaim> =
+            work_only_groth16_proof_receipt.inner.groth16().unwrap();
+        println!("G16 PROOF RECEIPT: {:?}", g16_proof_receipt);
+
+        let seal =
+            CircuitGroth16Proof::from_seal(g16_proof_receipt.seal.as_slice().try_into().unwrap());
+
+        let compressed_proof = seal.to_compressed().unwrap();
+
+        let commited_total_work: [u8; 16] = work_only_groth16_proof_receipt
+            .journal
+            .bytes
+            .try_into()
+            .unwrap();
+
+        let winternitz_details = generate_winternitz(&compressed_proof, &commited_total_work);
+
         let bridge_circuit_host_params = BridgeCircuitHostParams {
-            light_client_rpc_client,
-            citrea_rpc_client,
-            work_only_groth16_proof_receipt,
+            winternitz_details: vec![winternitz_details],
             spv,
             block_header_circuit_output,
         };
         println!("PROVING BRIDGE CIRCUIT");
-        prove_bridge_circuit(bridge_circuit_host_params).await;
+        prove_bridge_circuit(
+            bridge_circuit_host_params,
+            light_client_rpc_client,
+            citrea_rpc_client,
+        )
+        .await;
     }
 }
--- bridge-circuit-host/src/structs.rs
+++ bridge-circuit-host/src/structs.rs
@@ -1,11 +1,8 @@
-use alloy_rpc_client::RpcClient;
+use circuits_lib::bridge_circuit_core::winternitz::WinternitzHandler;
 use risc0_to_bitvm2_core::{header_chain::BlockHeaderCircuitOutput, spv::SPV};
-use risc0_zkvm::Receipt;
 
 pub struct BridgeCircuitHostParams {
-    pub light_client_rpc_client: RpcClient,
-    pub citrea_rpc_client: RpcClient,
-    pub work_only_groth16_proof_receipt: Receipt,
+    pub winternitz_details: Vec<WinternitzHandler>,
     pub spv: SPV,
     pub block_header_circuit_output: BlockHeaderCircuitOutput,
 }
