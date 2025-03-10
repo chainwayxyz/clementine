81c7057de5a901a7e21d5fbb36a5a5749c196559
Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>
2025-03-10 12:08:38 +0300

    Enhance bridge circuit api (#591)
    * Add new dependencies and utility functions for bridge circuit verification
    
    * feat: generate g16 proof from succinct proof and return public inputs from the bridge circuit API
    
    * feat: add production and test bridge circuit binaries, enhance build configuration, and improve verification logic
    
    * refactor: improve code readability and consistency in bridge circuit implementation
    
    * Remove warning
    
    * refactor: move parameters to test and update fetch_storage_proof function to use BCHostParameters
    
    * feat: update prove_bridge_circuit function to return ark_groth16 proof and refactor related logic
    
    * Add heavy guest side ops to host for test, use proof from BitVM
    
    * clippy
    
    * Update for new circom
    
    * feat: add bridge circuit build and test workflow with release profile
    
    * fix: clarify job name in bridge circuit build workflow
    
    * refactor: remove unused import in structs & fmt
    
    * refactor: remove RISC0_DEV_MODE environment variable from build workflow
    
    * fix: standardize shared-key naming in bridge circuit build workflow
    
    * fix: add TESTFLAGS environment variable for bridge circuit tests
    
    * refactor: remove prints
    
    * refactor: fmt + clippy
    
    * refactor: improve verification checks
    
    * refactor: rename bridge circuit core to bridge circuit common
    
    * refactor: rename bridge circuit core to bridge circuit common
    
    * refactor: update bridge circuit core references to bridge circuit common
    
    * refactor: update WORK_ONLY_IMAGE_ID to new hex literal
    
    ---------
    
    Co-authored-by: ozankaymak <ozan.kaymak@boun.edu.tr>

--- circuits-lib/src/bridge_circuit/mod.rs
+++ circuits-lib/src/bridge_circuit/mod.rs
@@ -4,25 +4,34 @@ pub mod lc_proof;
 pub mod storage_proof;
 
 use crate::bridge_circuit::groth16::CircuitGroth16WithTotalWork;
-use crate::bridge_circuit_core;
+use crate::bridge_circuit_common;
 use crate::common::zkvm::ZkvmGuest;
 use bitcoin::hashes::Hash;
-use bridge_circuit_core::groth16::CircuitGroth16Proof;
-use bridge_circuit_core::structs::BridgeCircuitInput;
-use bridge_circuit_core::winternitz::{verify_winternitz_signature, WinternitzHandler};
+use bridge_circuit_common::groth16::CircuitGroth16Proof;
+use bridge_circuit_common::structs::BridgeCircuitInput;
+use bridge_circuit_common::winternitz::{verify_winternitz_signature, WinternitzHandler};
 use lc_proof::lc_proof_verifier;
-use risc0_zkvm::guest::env;
 use sha2::{Digest, Sha256};
 use std::str::FromStr;
 use storage_proof::verify_storage_proofs;
 
+pub const HEADER_CHAIN_METHOD_ID: [u32; 8] = [
+    2421631365, 3264974484, 821027839, 1335612179, 1295879179, 713845602, 1229060261, 258954137,
+];
+
 pub fn verify_winternitz_and_groth16(input: &WinternitzHandler) -> bool {
-    let start = env::cycle_count();
-    let res = verify_winternitz_signature(input);
-    let end = env::cycle_count();
-    println!("WNV: {}", end - start);
-    res
+    verify_winternitz_signature(input)
 }
+
+/// TODO: Change this to a feature in the future
+pub const IS_TEST: bool = {
+    match option_env!("BRIDGE_CIRCUIT_MODE") {
+        Some(mode) if matches!(mode.as_bytes(), b"test") => true,
+        Some(mode) if matches!(mode.as_bytes(), b"prod") => false,
+        None => false,
+        _ => panic!("Invalid bridge circuit mode"),
+    }
+};
 
 pub fn convert_to_groth16_and_verify(message: &[u8], pre_state: &[u8; 32]) -> bool {
     let compressed_seal: [u8; 128] = match message[0..128].try_into() {
@@ -34,7 +43,6 @@ pub fn convert_to_groth16_and_verify(message: &[u8], pre_state: &[u8; 32]) -> bo
         Ok(total_work) => total_work,
         Err(_) => return false,
     };
-    println!("Total work: {:?}", total_work);
 
     let seal = match CircuitGroth16Proof::from_compressed(&compressed_seal) {
         Ok(seal) => seal,
@@ -42,17 +50,15 @@ pub fn convert_to_groth16_and_verify(message: &[u8], pre_state: &[u8; 32]) -> bo
     };
 
     let groth16_proof = CircuitGroth16WithTotalWork::new(seal, total_work);
-    let start = env::cycle_count();
-    let res = groth16_proof.verify(pre_state);
-    let end = env::cycle_count();
-    println!("G16V: {}", end - start);
-    println!("{}", res);
-    res
+    groth16_proof.verify(pre_state)
 }
 
 pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
-    let start = env::cycle_count();
     let input: BridgeCircuitInput = guest.read_from_host();
+    assert_eq!(HEADER_CHAIN_METHOD_ID, input.hcp.method_id);
+
+    // Verify the HCP
+    guest.verify(input.hcp.method_id, &input.hcp);
 
     let mut watchtower_flags: Vec<bool> = vec![];
     let mut wt_messages_with_idxs: Vec<(usize, Vec<u8>)> = vec![];
@@ -68,7 +74,11 @@ pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
             continue;
         }
 
-        let flag = verify_winternitz_signature(winternitz_handler);
+        let flag = if !IS_TEST {
+            verify_winternitz_signature(winternitz_handler)
+        } else {
+            true
+        };
         watchtower_flags.push(flag);
 
         if flag {
@@ -81,7 +91,7 @@ pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
     let mut total_work = [0u8; 32];
     for pair in wt_messages_with_idxs.iter() {
         // Grooth16 verification of work only circuit
-        if convert_to_groth16_and_verify(&pair.1, &work_only_image_id) {
+        if IS_TEST || convert_to_groth16_and_verify(&pair.1, &work_only_image_id) {
             total_work[16..32].copy_from_slice(
                 &pair.1[128..144]
                     .chunks_exact(4)
@@ -114,16 +124,15 @@ pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
     // MMR WILL BE FETCHED FROM LC PROOF WHEN IT IS READY - THIS IS JUST FOR PROOF OF CONCEPT
     let mmr = input.hcp.chain_state.block_hashes_mmr;
 
-    // SPV verification of payout transaction
-    println!("SPV verification {:?}", input.payout_spv.verify(mmr));
+    if !input.payout_spv.verify(mmr) {
+        panic!("Invalid SPV proof");
+    }
 
     // Light client proof verification
     let state_root = lc_proof_verifier(input.lcp.clone());
 
     // Storage proof verification for deposit tx index and withdrawal outpoint
     let user_wd_outpoint_str = verify_storage_proofs(&input.sp, state_root);
-
-    println!("User withdrawal outpoint: {:?}", user_wd_outpoint_str);
 
     let user_wd_outpoint = num_bigint::BigUint::from_str(&user_wd_outpoint_str).unwrap();
     let user_wd_txid = bitcoin::Txid::from_byte_array(
@@ -138,10 +147,6 @@ pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
         user_wd_txid,
         input.payout_spv.transaction.input[0].previous_output.txid
     );
-    println!(
-        "Payout transaction output: {:?}",
-        input.payout_spv.transaction.output
-    );
 
     let last_output = input.payout_spv.transaction.output.last().unwrap();
     let last_output_script = last_output.script_pubkey.to_bytes();
@@ -154,14 +159,14 @@ pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
     } else {
         operator_id[..len as usize].copy_from_slice(&last_output_script[2..(2 + len) as usize]);
     }
-    println!("Operator ID: {:?}", operator_id);
 
     let wintertniz_pubkeys_digest: [u8; 32] = Sha256::digest(&pub_key_concat).into();
     let pre_deposit_constant = [input.sp.txid_hex, wintertniz_pubkeys_digest, operator_id].concat();
 
     let deposit_constant: [u8; 32] = Sha256::digest(&pre_deposit_constant).into();
     let mut challenge_sending_watchtowers: [u8; 20] = [0u8; 20];
-    // Convert bools to bit flags
+
+    // Convert bools to bit flags (Actually no need for bools we can directly use the bit flags, but for clarity)
     for (i, &flag) in watchtower_flags.iter().enumerate() {
         if flag {
             challenge_sending_watchtowers[i / 8] |= 1 << (i % 8);
@@ -188,6 +193,4 @@ pub fn bridge_circuit(guest: &impl ZkvmGuest, work_only_image_id: [u8; 32]) {
     let journal_hash = blake3::hash(&concat_journal);
 
     guest.commit(journal_hash.as_bytes());
-    let end = env::cycle_count();
-    println!("WNT: {}", end - start);
 }
--- circuits-lib/src/work_only/mod.rs
+++ circuits-lib/src/work_only/mod.rs
@@ -1,12 +1,19 @@
-use crate::{bridge_circuit_core, common::zkvm::ZkvmGuest};
+use crate::{bridge_circuit_common, common::zkvm::ZkvmGuest};
 
-use bridge_circuit_core::structs::{WorkOnlyCircuitInput, WorkOnlyCircuitOutput};
+use bridge_circuit_common::structs::{WorkOnlyCircuitInput, WorkOnlyCircuitOutput};
 use crypto_bigint::{Encoding, U128, U256};
 use risc0_zkvm::guest::env;
 
+pub const HEADER_CHAIN_METHOD_ID: [u32; 8] = [
+    2421631365, 3264974484, 821027839, 1335612179, 1295879179, 713845602, 1229060261, 258954137,
+];
+
 pub fn work_only_circuit(guest: &impl ZkvmGuest) {
-    let start = env::cycle_count();
     let input: WorkOnlyCircuitInput = guest.read_from_host();
+    assert_eq!(
+        HEADER_CHAIN_METHOD_ID,
+        input.header_chain_circuit_output.method_id
+    );
     env::verify(
         input.header_chain_circuit_output.method_id,
         &borsh::to_vec(&input.header_chain_circuit_output).unwrap(),
@@ -15,7 +22,6 @@ pub fn work_only_circuit(guest: &impl ZkvmGuest) {
     let total_work_u256: U256 =
         U256::from_be_bytes(input.header_chain_circuit_output.chain_state.total_work);
     let (_, chain_state_total_work_u128): (U128, U128) = total_work_u256.into();
-    println!("Total work: {}", chain_state_total_work_u128);
     let mut words: [u32; 4] = chain_state_total_work_u128
         .to_le_bytes()
         .chunks_exact(4)
@@ -26,6 +32,4 @@ pub fn work_only_circuit(guest: &impl ZkvmGuest) {
 
     words.reverse();
     guest.commit(&WorkOnlyCircuitOutput { work_u128: words });
-    let end = env::cycle_count();
-    println!("WO: {}", end - start);
 }
