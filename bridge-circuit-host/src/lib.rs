a1ad2a076720307071fe0e5f0f82b7babeb56698
Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>
2025-03-13 16:54:13 +0300

    bridge circuit refactor tests and docs (#597)
    * refactor: remove bridge_circuit_common module
    
    * refactor: remove unused cargo.lock files
    
    * refactor: bridge circuit
    
    * rafactor: move spv creation and winternitz functions
    
    * refactor: enhance documentation and optimize SPV proof generation
    
    * refactor: improve documentation for light client and storage proof functions
    
    * refactor: update documentation
    
    * add tests + update docs + refactor
    
    * refactor: update documentation to specify Citrea RPC client for light client and storage proofs
    
    * refactor: replace 'unwrap' with 'expect' to explain why serialization cannot fail
    
    * refactor: replace hex decoding with direct u16 assignment for output length
    
    * refactor: replace hardcoded state root arrays with hex decoding for improved clarity
    
    * refactor: replace 'anyhow' with 'eyre'
    
    * refactor: change payment_block parameter type to bitcoin::Block
    
    * refactor: rename prove_work_only to prove_work_only_header_chain_proof for clarity
    
    * refactor: rename public_inputs function to generate_succinct_bridge_circuit_public_inputs for clarity
    
    * refactor: format function signatures for improved readability and consistency
    
    * refactor: clarify panic conditions and rename total_work variable

--- bridge-circuit-host/src/lib.rs
+++ bridge-circuit-host/src/lib.rs
@@ -2,8 +2,8 @@ use alloy::primitives::keccak256;
 use alloy_primitives::U256;
 use alloy_rpc_client::RpcClient;
 use alloy_rpc_types::EIP1186AccountProofResponse;
-use anyhow::bail;
-use circuits_lib::bridge_circuit_common::structs::{LightClientProof, StorageProof};
+use circuits_lib::bridge_circuit::structs::{LightClientProof, StorageProof};
+use eyre::bail;
 use hex::decode;
 use risc0_zkvm::{InnerReceipt, Receipt};
 use serde_json::json;
@@ -16,11 +16,55 @@ pub mod utils;
 
 const UTXOS_STORAGE_INDEX: [u8; 32] =
     hex_literal::hex!("0000000000000000000000000000000000000000000000000000000000000026");
+
 const DEPOSIT_MAPPING_STORAGE_INDEX: [u8; 32] =
     hex_literal::hex!("0000000000000000000000000000000000000000000000000000000000000027");
 
 const CONTRACT_ADDRESS: &str = "0x3100000000000000000000000000000000000002";
 
+/// Fetches the light client proof for a given L1 block height.
+///
+/// This function queries an Citrea light client prover RPC endpoint to obtain a light client proof
+/// for a specified L1 height. The proof is then decoded and deserialized into an `InnerReceipt`,
+/// which is further processed to extract a valid `Receipt`.
+///
+/// # Arguments
+/// * `l1_height` - A `u32` representing the L1 block height for which the proof is requested.
+/// * `client` - An instance of `RpcClient` used to send the JSON-RPC request.
+///
+/// # Returns
+/// * `Ok((LightClientProof, Receipt))` - A tuple containing:
+///   - `LightClientProof`: The extracted proof with journal bytes and L2 height.
+///   - `Receipt`: The transaction receipt parsed from the proof.
+/// * `Err(())` - If the function encounters an error at any stage.
+///
+/// # Panics
+/// This function will panic if:
+/// * The RPC response does not contain a valid `"proof"` string.
+/// * The proof is not a valid hex string.
+/// * The proof cannot be deserialized into `InnerReceipt`.
+/// * The receipt cannot be extracted from the `InnerReceipt`.
+/// * The RPC response does not contain a valid `"lastL2Height"` string.
+///
+/// Example:
+/// ```rust, ignore
+/// use alloy_rpc_client::RpcClient;
+/// use alloy_rpc_client::ClientBuilder;
+/// use bridge_circuit_host::fetch_light_client_proof;
+///
+/// const LIGHT_CLIENT_PROVER_URL: &str = "https://light-client-prover.testnet.citrea.xyz/";
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let light_client_rpc_client =
+///         ClientBuilder::default().http(LIGHT_CLIENT_PROVER_URL.parse().unwrap());
+///
+///     let (light_client_proof, lcp_receipt) =
+///         fetch_light_client_proof(72471, light_client_rpc_client)
+///             .await
+///             .unwrap();
+/// }
+/// ```
 pub async fn fetch_light_client_proof(
     l1_height: u32,
     client: RpcClient,
@@ -34,7 +78,11 @@ pub async fn fetch_light_client_proof(
         .await
         .unwrap();
 
-    let proof_str = response["proof"].as_str().expect("Proof is not a string")[2..].to_string();
+    let raw_proof = response["proof"].as_str().expect("Proof is missing");
+    let proof_str = raw_proof
+        .strip_prefix("0x")
+        .expect("Invalid proof format")
+        .to_string();
 
     let bytes = decode(proof_str).expect("Invalid hex");
     let decoded: InnerReceipt = bincode::deserialize(&bytes).expect("Failed to deserialize");
@@ -43,7 +91,6 @@ pub async fn fetch_light_client_proof(
     let l2_height = response["lightClientProofOutput"]["lastL2Height"]
         .as_str()
         .expect("l2 height is not a string");
-    println!("L2 height: {:?}", l2_height);
 
     Ok((
         LightClientProof {
@@ -54,6 +101,47 @@ pub async fn fetch_light_client_proof(
     ))
 }
 
+/// Fetches the storage proof for a given deposit index and transaction ID.
+///
+/// This function interacts with an Citrea RPC endpoint to retrieve a storage proof,
+/// which includes proof details for both the UTXO and the deposit index.
+///
+/// # Arguments
+/// * `l2_height` - A reference to `String` representing the L2 block height. (e.g. "0x123a")
+/// * `deposit_index` - A `u32` representing the deposit index.
+/// * `move_to_vault_txid` - A 32-byte array representing the transaction ID of the move-to-vault transaction.
+/// * `client` - An instance of `RpcClient` used to make the JSON-RPC request.
+///
+/// # Returns
+/// Returns a `StorageProof` struct containing serialized storage proofs for the UTXO and deposit index.
+///
+/// # Errors
+/// * This function will panic if:
+///   * `keccak256(UTXOS_STORAGE_INDEX)` does not return a valid 32-byte slice.
+///   * The RPC request to `eth_getProof` fails.
+///   * The response from the RPC call cannot be deserialized into an `EIP1186AccountProofResponse`.
+///
+/// Example:
+/// ```rust,ignore
+/// use alloy_rpc_client::ClientBuilder;
+/// use bridge_circuit_host::fetch_storage_proof;
+/// use hex_literal::hex;
+///
+/// const CITREA_TESTNET_RPC: &str = "https://rpc.testnet.citrea.xyz/";
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let citrea_rpc_client = ClientBuilder::default().http(CITREA_TESTNET_RPC.parse().unwrap());
+///
+///     let storage_proof = fetch_storage_proof(
+///         &"0xabc".to_string(),
+///         37,
+///         hex!("BB25103468A467382ED9F585129AD40331B54425155D6F0FAE8C799391EE2E7F"),
+///         citrea_rpc_client,
+///     )
+///     .await;
+/// }
+/// ```
 pub async fn fetch_storage_proof(
     l2_height: &String,
     deposit_index: u32,
@@ -64,24 +152,20 @@ pub async fn fetch_storage_proof(
     let tx_index: u32 = ind * 2;
 
     let storage_address_bytes = keccak256(UTXOS_STORAGE_INDEX);
-    println!("Storage address: {:?}", &storage_address_bytes[..]);
     let storage_address: U256 = U256::from_be_bytes(
         <[u8; 32]>::try_from(&storage_address_bytes[..]).expect("Slice with incorrect length"),
     );
+
+    // Storage key address calculation UTXO
     let storage_key: alloy_primitives::Uint<256, 4> = storage_address + U256::from(tx_index);
     let storage_key_hex = hex::encode(storage_key.to_be_bytes::<32>());
-    println!("Storage key: {:?}", &storage_key_hex);
     let storage_key_hex = format!("0x{}", storage_key_hex);
 
-    let concantenated = [move_to_vault_txid, DEPOSIT_MAPPING_STORAGE_INDEX].concat();
-
-    let storage_address_deposit = keccak256(concantenated);
+    // Storage key address calculation Deposit
+    let concatenated = [move_to_vault_txid, DEPOSIT_MAPPING_STORAGE_INDEX].concat();
+    let storage_address_deposit = keccak256(concatenated);
     let storage_address_deposit_hex = hex::encode(storage_address_deposit);
     let storage_address_deposit_hex = format!("0x{}", storage_address_deposit_hex);
-    println!(
-        "Storage address deposit: {:?}",
-        &storage_address_deposit_hex
-    );
 
     let request = json!([
         CONTRACT_ADDRESS,
@@ -92,9 +176,6 @@ pub async fn fetch_storage_proof(
     let response: serde_json::Value = client.request("eth_getProof", request).await.unwrap();
 
     let response: EIP1186AccountProofResponse = serde_json::from_value(response).unwrap();
-
-    println!("HOST VALUE INDEX: {:?}", &response.storage_proof[1].value);
-    println!("HOST VALUE MOVE TX: {:?}", &response.storage_proof[0].value);
 
     let serialized_utxo = serde_json::to_string(&response.storage_proof[0]).unwrap();
 
@@ -108,7 +189,22 @@ pub async fn fetch_storage_proof(
     }
 }
 
-fn receipt_from_inner(inner: InnerReceipt) -> anyhow::Result<Receipt> {
+/// Converts an `InnerReceipt` into a `Receipt`, ensuring all required fields are present.
+///
+/// # Arguments
+/// * `inner` - The `InnerReceipt` to extract data from.
+///
+/// # Returns
+/// Returns a `Receipt` if all required fields are found, otherwise returns an error.
+///
+/// # Errors
+/// This function can return an error in the following cases:
+/// * If `inner.claim()` is empty.
+/// * If `claim.value()` is empty.
+/// * If `claim.output.value()` is empty.
+/// * If `output` is `None`.
+/// * If `output.journal.value()` is empty.
+fn receipt_from_inner(inner: InnerReceipt) -> eyre::Result<Receipt> {
     let mb_claim = inner.claim().or_else(|_| bail!("Claim is empty"))?;
     let claim = mb_claim
         .value()
