09cb247e1c3c1f26141e5183cb671098ecec7524
Ekrem BAL <mail.ekrembal@gmail.com>
2025-03-21 16:33:44 +0300

    Add Simple CLI (#635)
    * Remove descriptor, add simple cli
    
    * Clementine CLI improvements (#637)
    
    * chore: Move the new cli bin to bin dir.
    
    * chore(bin/cli): Move code to handlers.
    
    * feat(bin/cli): Add verifier functions.
    
    * feat(bin/cli): Add GetDepositKeys.
    
    * Fix num_verifiers in BaseDeposit
    
    ---------
    
    Co-authored-by: Ceyhun Şen <ceyhuusen@gmail.com>

--- core/src/test/deposit_and_withdraw_e2e.rs
+++ core/src/test/deposit_and_withdraw_e2e.rs
@@ -1,10 +1,8 @@
 use super::common::citrea::BRIDGE_PARAMS;
-use crate::actor::Actor;
 use crate::bitvm_client::SECP;
 use crate::citrea::mock::MockCitreaClient;
 use crate::citrea::{CitreaClient, CitreaClientT, SATS_TO_WEI_MULTIPLIER};
 use crate::database::Database;
-use crate::musig2::AggregateFromPublicKeys;
 use crate::rpc::clementine::WithdrawParams;
 use crate::test::common::citrea::SECRET_KEYS;
 use crate::test::common::tx_utils::{
@@ -14,7 +12,6 @@ use crate::test::common::{
     create_regtest_rpc, generate_withdrawal_transaction_and_signature, mine_once_after_in_mempool,
     run_single_deposit,
 };
-use crate::{builder, EVMAddress};
 use crate::{
     extended_rpc::ExtendedRpc,
     test::common::{
@@ -25,7 +22,6 @@ use crate::{
 use alloy::primitives::FixedBytes;
 use alloy::primitives::U256;
 use async_trait::async_trait;
-use base64::Engine;
 use bitcoin::hashes::Hash;
 use bitcoin::{secp256k1::SecretKey, Address, Amount};
 use bitcoin::{OutPoint, Txid};
@@ -378,75 +374,6 @@ async fn citrea_deposit_and_withdraw_e2e() -> Result<()> {
         "chainwayxyz/citrea-test:60d9fd633b9e62b647039f913c6f7f8c085ad42e",
     );
     TestCaseRunner::new(CitreaDepositAndWithdrawE2E).run().await
-}
-
-#[tokio::test]
-#[ignore = "Manual testing utility"]
-async fn get_deposit_address_for_manual_tests() {
-    let mut config = create_test_config_with_thread_name().await;
-    let regtest = create_regtest_rpc(&mut config).await;
-    let rpc = regtest.rpc();
-
-    let signer = Actor::new(
-        config.secret_key,
-        config.winternitz_secret_key,
-        config.protocol_paramset().network,
-    );
-
-    let nofn_xonly_pk =
-        bitcoin::XOnlyPublicKey::from_musig2_pks(config.verifiers_public_keys.clone(), None)
-            .expect("Failed to create xonly pk");
-
-    let evm_address = EVMAddress([1u8; 20]);
-
-    let deposit_address = builder::address::generate_deposit_address(
-        nofn_xonly_pk,
-        signer.address.as_unchecked(),
-        evm_address,
-        config.protocol_paramset().bridge_amount,
-        config.protocol_paramset().network,
-        config.protocol_paramset().user_takes_after,
-    )
-    .unwrap();
-
-    // send a deposit tx
-    let deposit_outpoint = rpc
-        .send_to_address(&deposit_address.0, config.protocol_paramset().bridge_amount)
-        .await
-        .unwrap();
-
-    // wait until the deposit tx is in a block
-    rpc.mine_blocks(1).await.unwrap();
-
-    println!("Deposit address: {:?}", deposit_address);
-
-    let engine = base64::engine::GeneralPurpose::new(
-        &base64::alphabet::Alphabet::new(
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
-        )
-        .unwrap(),
-        base64::engine::general_purpose::PAD,
-    );
-
-    // gRPC request:
-    println!("grpcurl -plaintext -proto core/src/rpc/clementine.proto -d '{{");
-    println!("  \"deposit_outpoint\": {{");
-    println!(
-        "    \"txid\": \"{}\",",
-        engine.encode(deposit_outpoint.txid.to_byte_array())
-    );
-    println!("    \"vout\": {}", deposit_outpoint.vout);
-    println!("  }},");
-    println!("  \"evm_address\": \"{}\",", engine.encode(evm_address.0));
-    println!("  \"recovery_taproot_address\": \"{}\",", signer.address);
-    println!(
-        "  \"nofn_xonly_pk\": \"{}\"",
-        engine.encode(nofn_xonly_pk.serialize())
-    );
-    println!(
-        "}}' 127.0.0.1:{} clementine.ClementineAggregator.NewDeposit",
-        config.port
-    );
 }
 
 #[tokio::test]
