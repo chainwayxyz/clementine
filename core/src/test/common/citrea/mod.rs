2cb0ab3747ae67e76ed34f0f75d929adddde4bc4
Ceyhun Şen <ceyhuusen@gmail.com>
2025-03-25 17:56:10 +0300

    fix(tests/citrea): Fix endless loop in get_bridge_params.

--- core/src/test/common/citrea/mod.rs
+++ core/src/test/common/citrea/mod.rs
@@ -1,9 +1,7 @@
 //! # Citrea Related Utilities
 
-use crate::citrea::mock::MockCitreaClient;
 use crate::musig2::AggregateFromPublicKeys;
-use crate::rpc::clementine::Empty;
-use crate::{config::BridgeConfig, errors::BridgeError, test::common::create_actors};
+use crate::{config::BridgeConfig, errors::BridgeError};
 use bitcoin::secp256k1::PublicKey;
 use citrea_e2e::{
     bitcoin::BitcoinNode,
@@ -14,36 +12,29 @@ use citrea_e2e::{
 use jsonrpsee::http_client::HttpClient;
 pub use parameters::*;
 pub use requests::*;
-use tonic::Request;
 
 mod bitcoin_merkle;
 mod parameters;
 mod requests;
 
 /// Calculates bridge params dynamically with the N-of-N public key which
-/// retrieved from the aggregator setup.
-///
-/// CAUTION: This will create N-of-N public key using the default `BridgeConfig`!
+/// calculated from the verifier secret keys in `BridgeConfig::default`.
 pub fn get_bridge_params() -> String {
-    futures::executor::block_on(async move {
-        let config = BridgeConfig::default();
-        let (_, _, mut aggregator, _cleanup) = create_actors::<MockCitreaClient>(&config).await;
+    let config = BridgeConfig::default();
 
-        let verifiers_public_keys: Vec<PublicKey> = aggregator
-            .setup(Request::new(Empty {}))
-            .await
+    let verifiers_secret_keys = config.all_verifiers_secret_keys.unwrap();
+    let secp = bitcoin::secp256k1::Secp256k1::new();
+    let verifiers_public_keys: Vec<PublicKey> = verifiers_secret_keys
+        .iter()
+        .map(|sk| PublicKey::from_secret_key(&secp, sk))
+        .collect();
+
+    let nofn_xonly_pk =
+        bitcoin::XOnlyPublicKey::from_musig2_pks(verifiers_public_keys.clone(), None)
             .unwrap()
-            .into_inner()
-            .try_into()
-            .unwrap();
+            .to_string();
 
-        let nofn_xonly_pk =
-            bitcoin::XOnlyPublicKey::from_musig2_pks(verifiers_public_keys.clone(), None)
-                .unwrap()
-                .to_string();
-
-        format!("000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000000000000000002d4a20{}ac0063066369747265611400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a08000000003b9aca006800000000000000000000000000000000000000000000", nofn_xonly_pk)
-    })
+    format!("000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000000000000000002d4a20{}ac0063066369747265611400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a08000000003b9aca006800000000000000000000000000000000000000000000", nofn_xonly_pk)
 }
 
 /// Citrea e2e hardcoded EVM secret keys.
