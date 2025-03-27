d04d27087a319860c57723df8afe4d240a49322f
Ceyhun Şen <ceyhuusen@gmail.com>
2025-03-27 12:47:48 +0300

    chore(database): Remove deprecated header chain prover functions.

--- core/src/database/header_chain_prover.rs
+++ core/src/database/header_chain_prover.rs
@@ -9,9 +9,8 @@ use super::{
 };
 use crate::{errors::BridgeError, execute_query_with_tx};
 use bitcoin::{
-    block::{self, Header, Version},
-    hashes::Hash,
-    BlockHash, CompactTarget, TxMerkleNode,
+    block::{self, Header},
+    BlockHash,
 };
 use eyre::Context;
 use risc0_zkvm::Receipt;
@@ -35,76 +34,30 @@ impl Database {
         Ok(())
     }
 
-    /// Returns a block's hash and header, referring to it by it's height.
-    pub async fn get_block_info_by_height(
+    /// Gets the newest block's info that it's previous block has proven before.
+    /// This block will be the candidate block for the prover.
+    pub async fn get_non_proven_block(
         &self,
         tx: Option<DatabaseTransaction<'_, '_>>,
-        height: u64,
-    ) -> Result<(block::BlockHash, block::Header), BridgeError> {
+    ) -> Result<(BlockHash, Header, i32, Receipt), BridgeError> {
         let query = sqlx::query_as(
-            "SELECT block_hash, block_header FROM header_chain_proofs WHERE height = $1;",
-        )
-        .bind(height as i64);
-
-        let result: (Option<BlockHashDB>, Option<BlockHeaderDB>) =
-            execute_query_with_tx!(self.connection, tx, query, fetch_one)?;
-
-        match result {
-            (Some(hash), Some(header)) => Ok((hash.0, header.0)),
-            _ => Ok((
-                // TODO: Do we need to return all zeroed values or an error?
-                BlockHash::all_zeros(),
-                Header {
-                    version: Version::TWO,
-                    prev_blockhash: BlockHash::all_zeros(),
-                    merkle_root: TxMerkleNode::all_zeros(),
-                    time: 0,
-                    bits: CompactTarget::default(),
-                    nonce: 0,
-                },
-            )),
-        }
-    }
-
-    /// Returns a block's header, referring to it by it's height and hash.
-    pub async fn get_block_header(
-        &self,
-        tx: Option<DatabaseTransaction<'_, '_>>,
-        block_height: u64,
-        block_hash: BlockHash,
-    ) -> Result<Option<block::Header>, BridgeError> {
-        let query = sqlx::query_as(
-            "SELECT block_header FROM header_chain_proofs WHERE height = $1 AND block_hash = $2;",
-        )
-        .bind(block_height as i64)
-        .bind(BlockHashDB(block_hash));
-
-        let result: (Option<BlockHeaderDB>,) =
-            execute_query_with_tx!(self.connection, tx, query, fetch_one)?;
-
-        match result {
-            (Some(block_header),) => Ok(Some(block_header.0)),
-            (None,) => Ok(None),
-        }
-    }
-
-    /// Gets the block info of the latest block that has been saved to the
-    /// database.
-    pub async fn get_latest_block_info(
-        &self,
-        tx: Option<DatabaseTransaction<'_, '_>>,
-    ) -> Result<(u64, BlockHash), BridgeError> {
-        let query = sqlx::query_as(
-            "SELECT height, block_hash FROM header_chain_proofs ORDER BY height DESC;",
+            "SELECT h1.block_hash,
+                    h1.block_header,
+                    h1.height,
+                    h2.proof
+                FROM header_chain_proofs h1
+                JOIN header_chain_proofs h2 ON h1.prev_block_hash = h2.block_hash
+                WHERE h2.proof IS NOT NULL
+                ORDER BY h1.height
+                LIMIT 1;",
         );
 
-        let result: (Option<i32>, Option<BlockHashDB>) =
+        let result: (BlockHashDB, BlockHeaderDB, i32, Vec<u8>) =
             execute_query_with_tx!(self.connection, tx, query, fetch_one)?;
 
-        match result {
-            (Some(height), Some(hash)) => Ok((height as u64, hash.0)),
-            _ => Ok((0, BlockHash::all_zeros())),
-        }
+        let receipt: Receipt = borsh::from_slice(&result.3).wrap_err(BridgeError::BorshError)?;
+
+        Ok((result.0 .0, result.1 .0, result.2, receipt))
     }
 
     /// Sets an existing block's (in database) proof by referring to it by it's
@@ -146,32 +99,6 @@ impl Database {
 
         Ok(Some(receipt))
     }
-
-    /// Gets the newest block's info that it's previous block has proven before.
-    /// This block will be the candidate block for the prover.
-    pub async fn get_non_proven_block(
-        &self,
-        tx: Option<DatabaseTransaction<'_, '_>>,
-    ) -> Result<(BlockHash, Header, i32, Receipt), BridgeError> {
-        let query = sqlx::query_as(
-            "SELECT h1.block_hash,
-                    h1.block_header,
-                    h1.height,
-                    h2.proof
-                FROM header_chain_proofs h1
-                JOIN header_chain_proofs h2 ON h1.prev_block_hash = h2.block_hash
-                WHERE h2.proof IS NOT NULL
-                ORDER BY h1.height
-                LIMIT 1;",
-        );
-
-        let result: (BlockHashDB, BlockHeaderDB, i32, Vec<u8>) =
-            execute_query_with_tx!(self.connection, tx, query, fetch_one)?;
-
-        let receipt: Receipt = borsh::from_slice(&result.3).wrap_err(BridgeError::BorshError)?;
-
-        Ok((result.0 .0, result.1 .0, result.2, receipt))
-    }
 }
 
 #[cfg(test)]
@@ -189,6 +116,7 @@ mod tests {
         let config = create_test_config_with_thread_name().await;
         let db = Database::new(&config).await.unwrap();
 
+        // Set first block, so that get_non_proven_block won't return error.
         let block = block::Block {
             header: Header {
                 version: Version::TWO,
@@ -201,110 +129,35 @@ mod tests {
             txdata: vec![],
         };
         let block_hash = block.block_hash();
-        let height = 0x45;
+        let height = 1;
+        db.set_new_block(None, block_hash, block.header, height)
+            .await
+            .unwrap();
+        let receipt =
+            Receipt::try_from_slice(include_bytes!("../../tests/data/first_1.bin")).unwrap();
+        db.set_block_proof(None, block_hash, receipt).await.unwrap();
 
+        let block = block::Block {
+            header: Header {
+                version: Version::TWO,
+                prev_blockhash: block_hash,
+                merkle_root: TxMerkleNode::all_zeros(),
+                time: 1,
+                bits: CompactTarget::default(),
+                nonce: 1,
+            },
+            txdata: vec![],
+        };
+        let block_hash = block.block_hash();
+        let height = 2;
         db.set_new_block(None, block_hash, block.header, height)
             .await
             .unwrap();
 
-        let (read_block_hash, read_block_header) =
-            db.get_block_info_by_height(None, height).await.unwrap();
+        let (read_block_hash, read_block_header, _, _) =
+            db.get_non_proven_block(None).await.unwrap();
         assert_eq!(block_hash, read_block_hash);
         assert_eq!(block.header, read_block_header);
-    }
-
-    #[tokio::test]
-    async fn get_block_header() {
-        let config = create_test_config_with_thread_name().await;
-        let db = Database::new(&config).await.unwrap();
-
-        let block = block::Block {
-            header: Header {
-                version: Version::TWO,
-                prev_blockhash: BlockHash::all_zeros(),
-                merkle_root: TxMerkleNode::all_zeros(),
-                time: 0,
-                bits: CompactTarget::default(),
-                nonce: 0,
-            },
-            txdata: vec![],
-        };
-        let block_hash = block.block_hash();
-        let block_header = block.header;
-        let block_height = 0x45;
-
-        assert!(db
-            .get_block_header(None, block_height, block_hash)
-            .await
-            .is_err());
-
-        db.set_new_block(None, block_hash, block_header, block_height)
-            .await
-            .unwrap();
-        assert_eq!(
-            db.get_block_header(None, block_height, block_hash)
-                .await
-                .unwrap()
-                .unwrap(),
-            block_header
-        );
-    }
-
-    #[tokio::test]
-    #[serial_test::serial]
-    pub async fn get_latest_chain_proof_height() {
-        let config = create_test_config_with_thread_name().await;
-        let db = Database::new(&config).await.unwrap();
-
-        let mut block = block::Block {
-            header: Header {
-                version: Version::TWO,
-                prev_blockhash: BlockHash::all_zeros(),
-                merkle_root: TxMerkleNode::all_zeros(),
-                time: 0,
-                bits: CompactTarget::default(),
-                nonce: 0,
-            },
-            txdata: vec![],
-        };
-
-        assert!(db.get_latest_block_info(None).await.is_err());
-
-        // Adding a new block should return a height.
-        let height = 0x1F;
-        let hash = block.block_hash();
-        db.set_new_block(None, hash, block.header, height)
-            .await
-            .unwrap();
-        assert_eq!(
-            (height, hash),
-            db.get_latest_block_info(None).await.unwrap()
-        );
-
-        // Adding a new block with smaller height should not effect what's
-        // getting returned.
-        let smaller_height = height - 1;
-        block.header.time = 1; // To avoid same block hash.
-        db.set_new_block(None, block.block_hash(), block.header, smaller_height)
-            .await
-            .unwrap();
-        assert_eq!(
-            (height, hash),
-            db.get_latest_block_info(None).await.unwrap()
-        );
-
-        // Adding another block with higher height should return a different
-        // height.
-        let height = 0x45;
-        block.header.time = 2; // To avoid same block hash.
-        let hash = block.block_hash();
-        db.set_new_block(None, hash, block.header, height)
-            .await
-            .unwrap();
-        assert_eq!(
-            (height, hash),
-            db.get_latest_block_info(None).await.unwrap()
-        );
     }
 
     #[tokio::test]
