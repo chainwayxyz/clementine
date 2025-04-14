d92ea6dca3c05baea620f904f8c3a2494a8350c5
Ekrem BAL <mail.ekrembal@gmail.com>
2025-04-14 09:42:45 -0400

    fmt

--- core/src/test/common/mod.rs
+++ core/src/test/common/mod.rs
@@ -320,18 +320,40 @@ pub async fn run_single_deposit<C: CitreaClientT>(
     mine_once_after_in_mempool(&rpc, deposit_outpoint.txid, Some("Deposit outpoint"), None).await?;
     let deposit_blockhash = rpc.get_blockhash_of_tx(&deposit_outpoint.txid).await?;
 
-
     // print the tp of deposit tx
     let deposit_txid = deposit_outpoint.txid;
-    let transaction = rpc.client.get_raw_transaction(&deposit_txid, None).await.expect("a");
-    let tx_info: bitcoincore_rpc::json::GetRawTransactionResult = rpc.client.get_raw_transaction_info(&deposit_txid, None).await.expect("a");
-    let block: bitcoincore_rpc::json::GetBlockResult = rpc.client.get_block_info(&tx_info.blockhash.unwrap()).await.expect("a");
+    let transaction = rpc
+        .client
+        .get_raw_transaction(&deposit_txid, None)
+        .await
+        .expect("a");
+    let tx_info: bitcoincore_rpc::json::GetRawTransactionResult = rpc
+        .client
+        .get_raw_transaction_info(&deposit_txid, None)
+        .await
+        .expect("a");
+    let block: bitcoincore_rpc::json::GetBlockResult = rpc
+        .client
+        .get_block_info(&tx_info.blockhash.unwrap())
+        .await
+        .expect("a");
     let block_height = block.height;
-    let block = rpc.client.get_block(&tx_info.blockhash.unwrap()).await.expect("a");
-    let transaction_params = get_transaction_params(transaction.clone(), block, block_height as u32, deposit_txid);
+    let block = rpc
+        .client
+        .get_block(&tx_info.blockhash.unwrap())
+        .await
+        .expect("a");
+    let transaction_params = get_transaction_params(
+        transaction.clone(),
+        block,
+        block_height as u32,
+        deposit_txid,
+    );
     println!("Deposit tx Transaction params: {:?}", transaction_params);
-    println!("Deposit tx: {:?}", hex::encode(bitcoin::consensus::serialize(&transaction)));
-
+    println!(
+        "Deposit tx: {:?}",
+        hex::encode(bitcoin::consensus::serialize(&transaction))
+    );
 
     let nofn_xonly_pk =
         bitcoin::XOnlyPublicKey::from_musig2_pks(verifiers_public_keys.clone(), None).unwrap();
@@ -361,16 +383,34 @@ pub async fn run_single_deposit<C: CitreaClientT>(
 
     mine_once_after_in_mempool(&rpc, move_txid, Some("Move tx"), None).await?;
 
-    let transaction = rpc.client.get_raw_transaction(&move_txid, None).await.expect("a");
-    let tx_info: bitcoincore_rpc::json::GetRawTransactionResult = rpc.client.get_raw_transaction_info(&move_txid, None).await.expect("a");
-    let block: bitcoincore_rpc::json::GetBlockResult = rpc.client.get_block_info(&tx_info.blockhash.unwrap()).await.expect("a");
+    let transaction = rpc
+        .client
+        .get_raw_transaction(&move_txid, None)
+        .await
+        .expect("a");
+    let tx_info: bitcoincore_rpc::json::GetRawTransactionResult = rpc
+        .client
+        .get_raw_transaction_info(&move_txid, None)
+        .await
+        .expect("a");
+    let block: bitcoincore_rpc::json::GetBlockResult = rpc
+        .client
+        .get_block_info(&tx_info.blockhash.unwrap())
+        .await
+        .expect("a");
     let block_height = block.height;
-    let block = rpc.client.get_block(&tx_info.blockhash.unwrap()).await.expect("a");
-    let transaction_params = get_transaction_params(transaction.clone(), block, block_height as u32, move_txid);
+    let block = rpc
+        .client
+        .get_block(&tx_info.blockhash.unwrap())
+        .await
+        .expect("a");
+    let transaction_params =
+        get_transaction_params(transaction.clone(), block, block_height as u32, move_txid);
     println!("Move tx Transaction params: {:?}", transaction_params);
-    println!("Move tx: {:?}", hex::encode(bitcoin::consensus::serialize(&transaction)));
-
-
+    println!(
+        "Move tx: {:?}",
+        hex::encode(bitcoin::consensus::serialize(&transaction))
+    );
 
     Ok((
         verifiers,
