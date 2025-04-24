2f46c47dae81d005983638efe3451c5383021087
aoengin <ahmetoguzengin@hotmail.com>
2025-04-24 15:46:47 +0300

    generate data

--- core/src/test/common/tx_utils.rs
+++ core/src/test/common/tx_utils.rs
@@ -192,6 +192,11 @@ pub async fn send_tx_with_type(
         .iter()
         .find(|tx| tx.transaction_type == Some(tx_type.into()))
         .unwrap();
+
+    if tx_type == TxType::Kickoff {
+        tracing::info!("Raw kickoff tx: {:?}", round_tx.raw_tx.as_slice());
+    }
+    
     send_tx(tx_sender, rpc, round_tx.raw_tx.as_slice(), tx_type)
         .await
         .context(format!("failed to send {:?} transaction", tx_type))?;
