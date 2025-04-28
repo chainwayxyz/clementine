9b0f8f80734d14c736885c170447b535f14b8215
aoengin <ahmetoguzengin@hotmail.com>
2025-04-28 10:37:50 +0300

    update work-only elfs

--- risc0-circuits/bridge-circuit/guest/src/main.rs
+++ risc0-circuits/bridge-circuit/guest/src/main.rs
@@ -1,10 +1,10 @@
 use circuits_lib::{common, bridge_circuit::bridge_circuit};
 use hex_literal::hex;
 
-static MAINNET: [u8; 32] = hex!("b237f3513a6ef4bbcec303d0e3363e3f7bb2ccd2487b58bd730f7dfce2a44d64");
-static TESTNET4: [u8; 32] = hex!("49e267eafc5e424a11232fbd8e8fa084f77f07cf74352d4eab56c853565e2a66");
-static REGTEST: [u8; 32] = hex!("0e43ec107a9f73f6d3446150503e4cec83254ab596ee21ebc2706b511237ba1b");
-static SIGNET: [u8; 32] = hex!("d4c5c8b7e57df34fde68af32d8f90b42e25b72b490795cd164bc7fd0d6aee8a8");
+static MAINNET: [u8; 32] = hex!("c2a6fe36da915c98d003766ed0c3b048485be8194483cfee0694a76ca598443f");
+static TESTNET4: [u8; 32] = hex!("af9bddd502a4b7ea9600e02ec89e95f2b0f1637e0850662c0565e5af7947d081");
+static REGTEST: [u8; 32] = hex!("6858e5b435438b8e97a6b227847b9b00b369b41195386a1580ae1096dfe36acd");
+static SIGNET: [u8; 32] = hex!("a51794069e55d295c214f87f08d12f9b6a8548610b2e0fcf590c26b83918ea86");
 
 pub static WORK_ONLY_IMAGE_ID: [u8; 32] = match option_env!("BITCOIN_NETWORK") {
     Some(network) if matches!(network.as_bytes(), b"mainnet") => MAINNET,
