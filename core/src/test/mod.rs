3e7740f290546f07a8a310e8e343791aaa626e50
Mehmet Efe Akça <efeakca@gmail.com>
2025-05-22 16:02:23 +0300

    doc: add investigation of stack overflow

--- core/src/test/mod.rs
+++ core/src/test/mod.rs
@@ -12,9 +12,15 @@ mod bitvm_script;
 use ctor::ctor;
 
 #[ctor]
-// Increases stack to 32MB for tests, required due to bitvm
+// Increases stack to 32MB for tests, since tests fail with stack overflow otherwise.
 // Note that this is unsafe as using stdlib before `main` has no guarantees.
 // Read more: https://docs.rs/ctor/latest/ctor/attr.ctor.html
+//
+// After some investigation, the stack issue was narrowed down to `risc0-zkvm`s
+// prover. The CPU-based prover runs out of stack space in a parallelized accumulate
+// operation. FFI function is `risc0_circuit_rv32im_cpu_accum`, which is called
+// indirectly by `risc0-circuit-rv32im` in `src/prove/hal/mod.rs:205`. The stack usage
+// in the failing thread is ~384700 bytes.
 unsafe fn rust_min_stack() {
     std::env::set_var("RUST_MIN_STACK", "33554432");
 }
