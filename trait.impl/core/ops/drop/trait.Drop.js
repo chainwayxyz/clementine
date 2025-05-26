04a46a107ff3b995fd825725117513fdd7c3fa1b
ekrembal <mail.ekrembal@gmail.com>
2025-05-26 11:14:06 +0000

    Deploying to gh-pages from @ chainwayxyz/clementine@8474d3fde2608435caa18abae98c43f4d7a9c250 🚀

--- trait.impl/core/ops/drop/trait.Drop.js
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["clementine_core",[["impl&lt;T: <a class=\"trait\" href=\"clementine_core/states/context/trait.Owner.html\" title=\"trait clementine_core::states::context::Owner\">Owner</a> + 'static&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"clementine_core/task/manager/struct.BackgroundTaskManager.html\" title=\"struct clementine_core::task::manager::BackgroundTaskManager\">BackgroundTaskManager</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["clementine_core",[["impl&lt;T: <a class=\"trait\" href=\"clementine_core/states/context/trait.Owner.html\" title=\"trait clementine_core::states::context::Owner\">Owner</a> + 'static&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"clementine_core/task/manager/struct.BackgroundTaskManager.html\" title=\"struct clementine_core::task::manager::BackgroundTaskManager\">BackgroundTaskManager</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
