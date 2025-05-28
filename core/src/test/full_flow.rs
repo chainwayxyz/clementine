2de3ddd173f296c381c5bc2a79d24dff4c52a710
Ceyhun Şen <ceyhuusen@gmail.com>
2025-05-28 10:21:26 +0300

    Remove secret keys from bridge config and move it to test params (#798)
    * fix: Remove secret keys from bridge config and move it to test params.
    
    * chore: Make secret keys not optional.
    
    * chore: Don't clone secret keys.

--- core/src/test/full_flow.rs
+++ core/src/test/full_flow.rs
@@ -111,7 +111,12 @@ async fn base_setup(
     .await?;
     tracing::info!("Move transaction sent: {:x?}", move_tx_response.txid);
     let op0_xonly_pk = Actor::new(
-        config.all_operators_secret_keys.clone().unwrap()[0],
+        config
+            .test_params
+            .all_operators_secret_keys
+            .first()
+            .cloned()
+            .unwrap(),
         config.winternitz_secret_key,
         config.protocol_paramset().network,
     )
