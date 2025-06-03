158171a4bc6d6515b98c8223395e539807333ebc
Mehmet Efe Akça <efeakca@gmail.com>
2025-06-03 21:25:24 +0300

    fix: codespell

--- core/src/verifier.rs
+++ core/src/verifier.rs
@@ -1412,7 +1412,7 @@ where
 
             dbtx.commit().await?;
             tracing::info!(
-                "Commited watchtower challenge, commit data: {:?}",
+                "Committed watchtower challenge, commit data: {:?}",
                 commit_data
             );
         }
