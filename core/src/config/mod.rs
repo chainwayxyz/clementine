e49febdb1299ea2ad9f013c90fdb14b766a6fb78
Ceyhun Şen <ceyhuusen@gmail.com>
2025-06-03 15:55:07 +0300

    Scripts dir cleanup (#769)
    * chore: Move db sql to database module.
    
    * chore: Delete old hcp bins.
    
    * chore: Move test_config to config/ and rename.
    
    * chore: Delete old and unnecessary scripts.
    
    * chore: Readme fix.
    
    * chore: Move config files to test dir and move test dir into src/test
    
    * doc: Add scripts dir to readme.

--- core/src/config/mod.rs
+++ core/src/config/mod.rs
@@ -308,7 +308,7 @@ mod tests {
 
         // Read first example test file use for this test.
         let base_path = env!("CARGO_MANIFEST_DIR");
-        let config_path = format!("{}/tests/data/test_config.toml", base_path);
+        let config_path = format!("{}/src/test/data/bridge_config.toml", base_path);
         let content = fs::read_to_string(config_path).unwrap();
         let mut file = File::create(file_name).unwrap();
         file.write_all(content.as_bytes()).unwrap();
@@ -340,7 +340,7 @@ mod tests {
 
     #[test]
     fn test_test_config_parseable() {
-        let content = include_str!("../../tests/data/test_config.toml");
+        let content = include_str!("../test/data/bridge_config.toml");
         BridgeConfig::try_parse_from(content.to_string()).unwrap();
     }
 
