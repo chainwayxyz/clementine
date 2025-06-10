caf6c9a7310b1ee1936e6a00bab4de2a76bc073a
cetin <cetin@users.noreply.github.com>
2025-06-10 19:52:29 +0000

    Deploying to gh-pages from @ chainwayxyz/clementine@ffa8f06811a0dcc8a53bc572eeb47b7a89931b4f 🚀

--- trait.impl/circuits_lib/common/zkvm/trait.ZkvmHost.js
+++ trait.impl/circuits_lib/common/zkvm/trait.ZkvmHost.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bridge_circuit_host",[["impl ZkvmHost for <a class=\"struct\" href=\"bridge_circuit_host/mock_zkvm/struct.MockZkvmHost.html\" title=\"struct bridge_circuit_host::mock_zkvm::MockZkvmHost\">MockZkvmHost</a>"]]]]);
+    var implementors = Object.fromEntries([["bridge_circuit_host",[["impl <a class=\"trait\" href=\"circuits_lib/common/zkvm/trait.ZkvmHost.html\" title=\"trait circuits_lib::common::zkvm::ZkvmHost\">ZkvmHost</a> for <a class=\"struct\" href=\"bridge_circuit_host/mock_zkvm/struct.MockZkvmHost.html\" title=\"struct bridge_circuit_host::mock_zkvm::MockZkvmHost\">MockZkvmHost</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[210]}
+//{"start":57,"fragment_lengths":[340]}
