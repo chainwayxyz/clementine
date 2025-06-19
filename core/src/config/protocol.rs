17d13d4c5beee1019efa54546a62326a9ed65998
atacann <atacaniyidogan@gmail.com>
2025-06-19 16:10:40 +0300

    test nonstandard

--- core/src/config/protocol.rs
+++ core/src/config/protocol.rs
@@ -277,5 +277,5 @@ pub const REGTEST_PARAMSET: ProtocolParamset = ProtocolParamset {
         135, 127, 96, 197, 209, 59, 13, 243, 184, 10, 25, 163, 197, 237, 43, 164, 90, 184, 43, 190,
         122, 88, 234, 82, 78, 92, 249, 255, 206, 153, 87, 255,
     ],
-    bridge_nonstandard: false,
+    bridge_nonstandard: true,
 };
