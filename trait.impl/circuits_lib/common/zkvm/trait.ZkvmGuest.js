3480dfecbddc34c3536dbf44756b4c5fa1e31a27
aoengin <85455415+aoengin@users.noreply.github.com>
2025-07-01 09:48:10 +0000

    Deploying to gh-pages from @ chainwayxyz/clementine@6319c734b8173e331e0aee14b1d9a76694bdc82d 🚀

--- trait.impl/circuits_lib/common/zkvm/trait.ZkvmGuest.js
+++ trait.impl/circuits_lib/common/zkvm/trait.ZkvmGuest.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bridge_circuit_host",[["impl ZkvmGuest for <a class=\"struct\" href=\"bridge_circuit_host/mock_zkvm/struct.MockZkvmHost.html\" title=\"struct bridge_circuit_host::mock_zkvm::MockZkvmHost\">MockZkvmHost</a>"]]],["circuits_lib",[]]]);
+    var implementors = Object.fromEntries([["bridge_circuit_host",[["impl <a class=\"trait\" href=\"circuits_lib/common/zkvm/trait.ZkvmGuest.html\" title=\"trait circuits_lib::common::zkvm::ZkvmGuest\">ZkvmGuest</a> for <a class=\"struct\" href=\"bridge_circuit_host/mock_zkvm/struct.MockZkvmHost.html\" title=\"struct bridge_circuit_host::mock_zkvm::MockZkvmHost\">MockZkvmHost</a>"]]],["circuits_lib",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[211,20]}
+//{"start":57,"fragment_lengths":[343,20]}
