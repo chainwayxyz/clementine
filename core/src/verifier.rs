161f09ab2402ec647bb59fdf21c3ace64e7186cf
atacann <atacaniyidogan@gmail.com>
2025-07-23 18:48:35 +0300

    delete dup code

--- core/src/verifier.rs
+++ core/src/verifier.rs
@@ -1113,7 +1113,7 @@ where
     /// Recover the address from the signature
     /// EIP712 hash is calculated from optimistic payout params
     /// Signature is the signature of the eip712 hash
-    pub fn recover_address_from_signature(
+    pub fn recover_address_from_ecdsa_signature(
         deposit_id: u32,
         input_signature: Signature,
         input_outpoint: OutPoint,
@@ -1157,22 +1157,15 @@ where
         if let Some(address_in_config) = self.config.opt_payout_verification_address {
             // check if verification signature is provided by aggregator
             if let Some(verification_signature) = verification_signature {
-                let input_sig_bytes = input_signature.serialize().to_vec();
-                let outpoint_txid_bytes = input_outpoint.txid.to_byte_array();
-                let script_pubkey_bytes = output_script_pubkey.as_bytes().to_vec();
-                let params = ClementineOptimisticPayoutMessage {
-                    withdrawal_id: deposit_id,
-                    input_signature: input_sig_bytes.into(),
-                    input_outpoint_txid: outpoint_txid_bytes.into(),
-                    input_outpoint_vout: input_outpoint.vout,
-                    output_script_pubkey: script_pubkey_bytes.into(),
-                    output_amount: output_amount.to_sat(),
-                };
-                let eip712_hash = params.eip712_signing_hash(&DOMAIN);
+                let address_from_sig = Self::recover_address_from_ecdsa_signature(
+                    deposit_id,
+                    input_signature,
+                    input_outpoint,
+                    output_script_pubkey.clone(),
+                    output_amount,
+                    verification_signature,
+                )?;
 
-                let address_from_sig = verification_signature
-                    .recover_address_from_prehash(&eip712_hash)
-                    .wrap_err("Invalid signature")?;
                 // check if verification signature is signed by the address in config
                 if address_from_sig != address_in_config {
                     return Err(BridgeError::InvalidOptPayoutVerificationSignature);
@@ -2968,7 +2961,7 @@ mod tests {
                 .unwrap();
         let output_amount = Amount::from_sat(1000000000000000000);
         let deposit_id = 1;
-        let address = Verifier::<MockCitreaClient>::recover_address_from_signature(
+        let address = Verifier::<MockCitreaClient>::recover_address_from_ecdsa_signature(
             deposit_id,
             input_signature,
             input_outpoint,
