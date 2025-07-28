a4725ebc7d0cb27c1a10f56ea9e653bcdea34374
Mehmet Efe Akça <efeakca@gmail.com>
2025-07-28 19:05:40 +0300

    fix: address comments

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -9,6 +9,7 @@ use http::HeaderValue;
 use serde::{Deserialize, Serialize};
 use std::fmt::{Debug, Display};
 use std::future::Future;
+use std::ops::Index;
 use std::pin::Pin;
 use std::sync::Arc;
 use std::task::{Context, Poll};
@@ -390,18 +391,23 @@ pub struct RbfSigningInfo {
     pub tweak_merkle_root: Option<TapNodeHash>,
 }
 pub trait Last20Bytes {
-    fn last_20_bytes(self) -> [u8; 20];
+    fn last_20_bytes(&self) -> [u8; 20];
 }
 
 pub trait TryLast20Bytes {
     fn try_last_20_bytes(self) -> Result<[u8; 20], BridgeError>;
 }
 
+const fn larger_than_20<const N: usize>() {
+    if N < 20 {
+        panic!("Input is too short to contain 20 bytes");
+    }
+    ()
+}
+
 impl Last20Bytes for [u8; 32] {
-    fn last_20_bytes(self) -> [u8; 20] {
-        let mut result = [0u8; 20];
-        result.copy_from_slice(&self[12..32]);
-        result
+    fn last_20_bytes(&self) -> [u8; 20] {
+        self.try_last_20_bytes().expect("will not happen")
     }
 }
 
@@ -411,28 +417,22 @@ pub trait ScriptBufExt {
 
 impl ScriptBufExt for ScriptBuf {
     fn try_get_taproot_pk(&self) -> Result<XOnlyPublicKey, BridgeError> {
-        if WitnessVersion::try_from(self.first_opcode().wrap_err("Script is empty")?)
-            .wrap_err("Script's first opcode is not a valid witness version")?
-            != WitnessVersion::V1
-        {
-            return Err(
-                eyre::eyre!("Script is not a valid P2TR script (not Witness Version 1)").into(),
-            );
-        }
-        if self.len() != 34 {
+        if !self.is_p2tr() {
             return Err(eyre::eyre!("Script is not a valid P2TR script (not 34 bytes)").into());
         }
+
         Ok(XOnlyPublicKey::from_slice(&self.as_bytes()[2..34])
             .wrap_err("Failed to parse XOnlyPublicKey from script")?)
     }
 }
 
-impl TryLast20Bytes for Vec<u8> {
+impl TryLast20Bytes for &[u8] {
     fn try_last_20_bytes(self) -> Result<[u8; 20], BridgeError> {
         if self.len() < 20 {
             return Err(eyre::eyre!("Input is too short to contain 20 bytes").into());
         }
         let mut result = [0u8; 20];
+
         result.copy_from_slice(&self[self.len() - 20..]);
         Ok(result)
     }
