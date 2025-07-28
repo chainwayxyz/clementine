4dbb51d28529fafe9407191218da77537ef9f923
Ceyhun Şen <ceyhuusen@gmail.com>
2025-07-28 11:35:49 +0300

    fix: Compilation error.

--- core/src/main.rs
+++ core/src/main.rs
@@ -6,8 +6,6 @@
 //! spawn multiple actor servers that it needs, in different processes. Meaning
 //! Clementine binary should be run multiple times with different arguments.
 
-use std::{str::FromStr as _, time::Duration};
-
 use clementine_core::{
     bitvm_client::{load_or_generate_bitvm_cache, BITVM_CACHE},
     citrea::CitreaClient,
@@ -18,6 +16,7 @@ use clementine_core::{
     },
     utils::{initialize_logger, initialize_telemetry},
 };
+use std::str::FromStr as _;
 use tracing::{level_filters::LevelFilter, Level};
 
 #[tokio::main]
@@ -74,7 +73,7 @@ async fn main() {
             create_aggregator_grpc_server(config.clone())
                 .await
                 .expect("Can't create aggregator server")
-                .1;
+                .1
         }
     };
     println!("Server has started successfully.");
