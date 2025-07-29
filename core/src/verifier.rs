7684945ae60a868f7738876b0eda29b5309b7c6c
Ahmet Oguz Engin <85455415+aoengin@users.noreply.github.com>
2025-07-29 12:37:25 +0300

    CMT-22 (#959)
    * Optimize Winternitz key validation by performing checks before expensive processing
    
    * fmt
    
    ---------
    
    Co-authored-by: Ozan Kaymak <92448699+ozankaymak@users.noreply.github.com>

--- core/src/verifier.rs
+++ core/src/verifier.rs
@@ -1169,27 +1169,27 @@ where
             )
             .await?;
 
-        let winternitz_keys: Vec<winternitz::PublicKey> = keys
-            .winternitz_pubkeys
-            .into_iter()
-            .map(|x| x.try_into())
-            .collect::<Result<_, BridgeError>>()?;
-
-        if winternitz_keys.len() != ClementineBitVMPublicKeys::number_of_flattened_wpks() {
+        if keys.winternitz_pubkeys.len() != ClementineBitVMPublicKeys::number_of_flattened_wpks() {
             tracing::error!(
                 "Invalid number of winternitz keys received from operator {:?}: got: {} expected: {}",
                 operator_xonly_pk,
-                winternitz_keys.len(),
+                keys.winternitz_pubkeys.len(),
                 ClementineBitVMPublicKeys::number_of_flattened_wpks()
             );
             return Err(eyre::eyre!(
                 "Invalid number of winternitz keys received from operator {:?}: got: {} expected: {}",
                 operator_xonly_pk,
-                winternitz_keys.len(),
+                keys.winternitz_pubkeys.len(),
                 ClementineBitVMPublicKeys::number_of_flattened_wpks()
             )
             .into());
         }
+
+        let winternitz_keys: Vec<winternitz::PublicKey> = keys
+            .winternitz_pubkeys
+            .into_iter()
+            .map(|x| x.try_into())
+            .collect::<Result<_, BridgeError>>()?;
 
         let bitvm_pks = ClementineBitVMPublicKeys::from_flattened_vec(&winternitz_keys);
 
