57e6c0edc93c8a0a5966cd68db9dcec7e3e0b033
Ceyhun Şen <ceyhuusen@gmail.com>
2025-07-31 10:19:27 +0300

    fix: Remove deleted config's test.

--- core/src/config/mod.rs
+++ core/src/config/mod.rs
@@ -385,12 +385,4 @@ mod tests {
         let content = include_str!("../test/data/bridge_config.toml");
         BridgeConfig::try_parse_from(content.to_string()).unwrap();
     }
-
-    #[test]
-    fn test_docker_config_parseable() {
-        let content = include_str!("../../../scripts/docker/configs/regtest/bridge_config.toml");
-        BridgeConfig::try_parse_from(content.to_string()).unwrap();
-        let content = include_str!("../../../scripts/docker/configs/testnet4/bridge_config.toml");
-        BridgeConfig::try_parse_from(content.to_string()).unwrap();
-    }
 }
