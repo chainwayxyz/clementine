2ad11ff93686b65799ea8f66dd14ee492024bec5
ekrembal <mail.ekrembal@gmail.com>
2025-08-01 18:03:54 +0000

    Deploying to gh-pages from @ chainwayxyz/clementine@fa1a17086793826cfba0c52dfdf3accc3a092d4c 🚀

--- trait.impl/alloy_sol_types/types/ty/trait.SolType.js
+++ trait.impl/alloy_sol_types/types/ty/trait.SolType.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["clementine_core",[["impl SolType for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.MerkleProof.html\" title=\"struct clementine_core::citrea::Bridge::MerkleProof\">MerkleProof</a>"],["impl SolType for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.Transaction.html\" title=\"struct clementine_core::citrea::Bridge::Transaction\">Transaction</a>"],["impl SolType for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.UTXO.html\" title=\"struct clementine_core::citrea::Bridge::UTXO\">UTXO</a>"]]]]);
+    var implementors = Object.fromEntries([["clementine_core",[["impl SolType for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.MerkleProof.html\" title=\"struct clementine_core::citrea::Bridge::MerkleProof\">MerkleProof</a>"],["impl SolType for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.Transaction.html\" title=\"struct clementine_core::citrea::Bridge::Transaction\">Transaction</a>"],["impl SolType for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.UTXO.html\" title=\"struct clementine_core::citrea::Bridge::UTXO\">UTXO</a>"],["impl SolType for <a class=\"struct\" href=\"clementine_core/verifier/struct.ClementineOptimisticPayoutMessage.html\" title=\"struct clementine_core::verifier::ClementineOptimisticPayoutMessage\">ClementineOptimisticPayoutMessage</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[546]}
+//{"start":57,"fragment_lengths":[783]}
--- trait.impl/alloy_sol_types/types/value/trait.SolValue.js
+++ trait.impl/alloy_sol_types/types/value/trait.SolValue.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["clementine_core",[["impl SolValue for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.MerkleProof.html\" title=\"struct clementine_core::citrea::Bridge::MerkleProof\">MerkleProof</a>"],["impl SolValue for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.Transaction.html\" title=\"struct clementine_core::citrea::Bridge::Transaction\">Transaction</a>"],["impl SolValue for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.UTXO.html\" title=\"struct clementine_core::citrea::Bridge::UTXO\">UTXO</a>"]]]]);
+    var implementors = Object.fromEntries([["clementine_core",[["impl SolValue for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.MerkleProof.html\" title=\"struct clementine_core::citrea::Bridge::MerkleProof\">MerkleProof</a>"],["impl SolValue for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.Transaction.html\" title=\"struct clementine_core::citrea::Bridge::Transaction\">Transaction</a>"],["impl SolValue for <a class=\"struct\" href=\"clementine_core/citrea/Bridge/struct.UTXO.html\" title=\"struct clementine_core::citrea::Bridge::UTXO\">UTXO</a>"],["impl SolValue for <a class=\"struct\" href=\"clementine_core/verifier/struct.ClementineOptimisticPayoutMessage.html\" title=\"struct clementine_core::verifier::ClementineOptimisticPayoutMessage\">ClementineOptimisticPayoutMessage</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[549]}
+//{"start":57,"fragment_lengths":[787]}
