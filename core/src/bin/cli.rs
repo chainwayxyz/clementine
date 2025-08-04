fe5153fb108f0f058da05137ac2f39a5cb5e1a58
Ekrem BAL <mail.ekrembal@gmail.com>
2025-08-04 14:07:04 -0400

    Update cli command for getting emergency stop tx

--- core/src/bin/cli.rs
+++ core/src/bin/cli.rs
@@ -3,12 +3,11 @@
 use std::path::PathBuf;
 use std::str::FromStr;
 
-use bitcoin::{hashes::Hash, Block, ScriptBuf, Txid};
+use bitcoin::{hashes::Hash, ScriptBuf, Txid};
 use clap::{Parser, Subcommand};
 use clementine_core::{
     config::BridgeConfig,
     deposit::SecurityCouncil,
-    errors::BridgeError,
     rpc::clementine::{
         self, clementine_aggregator_client::ClementineAggregatorClient, deposit::DepositData,
         Actors, BaseDeposit, Deposit, Empty, GetEntityStatusesRequest, Outpoint,
@@ -16,8 +15,6 @@ use clementine_core::{
     },
     EVMAddress,
 };
-use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
 use tonic::Request;
 
 #[derive(Parser)]
@@ -166,17 +163,6 @@ enum AggregatorCommands {
         #[arg(long)]
         user_takes_after: Option<u64>,
     },
-    /// Get transaction parameters of a move transaction
-    GetTxParamsOfMoveTx {
-        #[arg(long)]
-        bitcoin_rpc_url: String,
-        #[arg(long)]
-        bitcoin_rpc_user: String,
-        #[arg(long)]
-        bitcoin_rpc_password: String,
-        #[arg(long)]
-        move_txid: String,
-    },
     GetReplacementDepositAddress {
         #[arg(long)]
         move_txid: String,
@@ -205,182 +191,37 @@ enum AggregatorCommands {
         #[arg(long)]
         restart_tasks: Option<bool>,
     },
+    /// Internal command to get the emergency stop encryption public key
+    InternalGetEmergencyStopTx {
+        #[arg(long)]
+        /// A comma-separated list of move txids
+        move_txids: String,
+    },
     /// Get vergen build information
     Vergen,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct BitcoinMerkleTree {
-    depth: u32,
-    nodes: Vec<Vec<[u8; 32]>>,
-}
-
-impl BitcoinMerkleTree {
-    pub fn new(transactions: Vec<[u8; 32]>) -> Self {
-        // assert!(depth > 0, "Depth must be greater than 0");
-        // assert!(depth <= 254, "Depth must be less than or equal to 254");
-        // assert!(
-        //     u32::pow(2, (depth) as u32) >= transactions.len() as u32,
-        //     "Too many transactions for this depth"
-        // );
-        let depth = (transactions.len() - 1).ilog(2) + 1;
-        let mut tree = BitcoinMerkleTree {
-            depth,
-            nodes: vec![],
-        };
-
-        // Populate leaf nodes
-        tree.nodes.push(vec![]);
-        for tx in transactions.iter() {
-            tree.nodes[0].push(*tx);
-        }
-
-        // Construct the tree
-        let mut curr_level_offset: usize = 1;
-        let mut prev_level_size = transactions.len();
-        let mut prev_level_index_offset = 0;
-        let mut preimage: [u8; 64] = [0; 64];
-        while prev_level_size > 1 {
-            // println!("curr_level_offset: {}", curr_level_offset);
-            // println!("prev_level_size: {}", prev_level_size);
-            // println!("prev_level_index_offset: {}", prev_level_index_offset);
-            tree.nodes.push(vec![]);
-            for i in 0..(prev_level_size / 2) {
-                preimage[..32].copy_from_slice(
-                    &tree.nodes[curr_level_offset - 1_usize][prev_level_index_offset + i * 2],
-                );
-                preimage[32..].copy_from_slice(
-                    &tree.nodes[curr_level_offset - 1][prev_level_index_offset + i * 2 + 1],
-                );
-                let combined_hash = calculate_double_sha256(&preimage);
-                tree.nodes[curr_level_offset].push(combined_hash);
-            }
-            if prev_level_size % 2 == 1 {
-                let mut preimage: [u8; 64] = [0; 64];
-                preimage[..32].copy_from_slice(
-                    &tree.nodes[curr_level_offset - 1]
-                        [prev_level_index_offset + prev_level_size - 1],
-                );
-                preimage[32..].copy_from_slice(
-                    &tree.nodes[curr_level_offset - 1]
-                        [prev_level_index_offset + prev_level_size - 1],
-                );
-                let combined_hash = calculate_double_sha256(&preimage);
-                tree.nodes[curr_level_offset].push(combined_hash);
-            }
-            curr_level_offset += 1;
-            prev_level_size = prev_level_size.div_ceil(2);
-            prev_level_index_offset = 0;
-        }
-        tree
-    }
-
-    // Returns the Merkle root
-    pub fn root(&self) -> [u8; 32] {
-        self.nodes[self.nodes.len() - 1][0]
-    }
-
-    pub fn get_idx_path(&self, index: u32) -> Vec<[u8; 32]> {
-        assert!(index < self.nodes[0].len() as u32, "Index out of bounds");
-        let mut path = vec![];
-        let mut level = 0;
-        let mut i = index;
-        while level < self.nodes.len() as u32 - 1 {
-            if i % 2 == 1 {
-                path.push(self.nodes[level as usize][i as usize - 1]);
-            } else if (self.nodes[level as usize].len() - 1) as u32 == i {
-                path.push(self.nodes[level as usize][i as usize]);
-            } else {
-                path.push(self.nodes[level as usize][(i + 1) as usize]);
-            }
-
-            level += 1;
-            i /= 2;
-        }
-
-        path
-    }
-
-    pub fn calculate_root_with_merkle_proof(
-        &self,
-        txid: [u8; 32],
-        idx: u32,
-        merkle_proof: Vec<[u8; 32]>,
-    ) -> [u8; 32] {
-        let mut preimage: [u8; 64] = [0; 64];
-        let mut combined_hash: [u8; 32] = txid;
-        let mut index = idx;
-        let mut level: u32 = 0;
-        while level < self.depth {
-            if index % 2 == 0 {
-                preimage[..32].copy_from_slice(&combined_hash);
-                preimage[32..].copy_from_slice(&merkle_proof[level as usize]);
-                combined_hash = calculate_double_sha256(&preimage);
-            } else {
-                preimage[..32].copy_from_slice(&merkle_proof[level as usize]);
-                preimage[32..].copy_from_slice(&combined_hash);
-                combined_hash = calculate_double_sha256(&preimage);
-            }
-            level += 1;
-            index /= 2;
-        }
-        combined_hash
-    }
-}
-
-pub fn calculate_double_sha256(input: &[u8]) -> [u8; 32] {
-    let mut hasher = Sha256::default();
-    hasher.update(input);
-    let result = hasher.finalize_reset();
-    hasher.update(result);
-    hasher.finalize().into()
-}
-
-fn _get_block_merkle_proof(
-    block: Block,
-    target_txid: Txid,
-) -> Result<(usize, Vec<u8>), BridgeError> {
-    let mut txid_index = 0;
-    let txids = block
-        .txdata
-        .iter()
-        .enumerate()
-        .map(|(i, tx)| {
-            if tx.compute_txid() == target_txid {
-                txid_index = i;
-            }
-
-            if i == 0 {
-                [0; 32]
-            } else {
-                let wtxid = tx.compute_wtxid();
-                wtxid.as_byte_array().to_owned()
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let merkle_tree = BitcoinMerkleTree::new(txids.clone());
-    let _witness_root = block.witness_root().expect("Failed to get witness root");
-    let witness_idx_path =
-        merkle_tree.get_idx_path(txid_index.try_into().expect("Failed to convert index"));
-
-    let _root = merkle_tree.calculate_root_with_merkle_proof(
-        txids[txid_index],
-        txid_index.try_into().expect("Failed to convert index"),
-        witness_idx_path.clone(),
-    );
-
-    Ok((txid_index, witness_idx_path.into_iter().flatten().collect()))
-}
-
 // Create a minimal config with default TLS paths
 fn create_minimal_config() -> BridgeConfig {
+    // CLIENT_KEY_PATH env var will be used if it is set
+    // CLIENT_CERT_PATH env var will be used if it is set
+    // CA_CERT_PATH env var will be used if it is set
     BridgeConfig {
-        server_cert_path: PathBuf::from("certs/server/server.pem"),
-        server_key_path: PathBuf::from("certs/server/server.key"),
-        ca_cert_path: PathBuf::from("certs/ca/ca.pem"),
-        client_cert_path: PathBuf::from("certs/client/client.pem"),
-        client_key_path: PathBuf::from("certs/client/client.key"),
+        server_cert_path: PathBuf::from(
+            std::env::var("CLIENT_CERT_PATH").unwrap_or("certs/server/server.pem".to_string()),
+        ),
+        server_key_path: PathBuf::from(
+            std::env::var("CLIENT_KEY_PATH").unwrap_or("certs/server/server.key".to_string()),
+        ),
+        ca_cert_path: PathBuf::from(
+            std::env::var("CA_CERT_PATH").unwrap_or("certs/ca/ca.pem".to_string()),
+        ),
+        client_cert_path: PathBuf::from(
+            std::env::var("CLIENT_CERT_PATH").unwrap_or("certs/client/client.pem".to_string()),
+        ),
+        client_key_path: PathBuf::from(
+            std::env::var("CLIENT_KEY_PATH").unwrap_or("certs/client/client.key".to_string()),
+        ),
         client_verification: true,
         ..Default::default()
     }
@@ -917,125 +758,39 @@ async fn handle_aggregator_call(url: String, command: AggregatorCommands) {
 
             println!("Deposit address: {}", deposit_address.0);
         }
-        AggregatorCommands::GetTxParamsOfMoveTx {
-            bitcoin_rpc_url: _,
-            bitcoin_rpc_user: _,
-            bitcoin_rpc_password: _,
-            move_txid: _,
-        } => {
-            unimplemented!()
-            // let extended_rpc = extended_rpc::ExtendedRpc::connect(
-            //     bitcoin_rpc_url,
-            //     bitcoin_rpc_user,
-            //     bitcoin_rpc_password,
-            // )
-            // .await
-            // .expect("Failed to connect to Bitcoin RPC");
-
-            // let flag: u16 = 1;
-
-            // let tx_id: &bitcoin::Txid =
-            //     &bitcoin::Txid::from_str(&move_txid).expect("Failed to parse txid");
-
-            // let tx = extended_rpc
-            //     .get_tx_of_txid(tx_id)
-            //     .await
-            //     .expect("Failed to get tx of txid");
-
-            // let block_hash = extended_rpc
-            //     .get_blockhash_of_tx(tx_id)
-            //     .await
-            //     .expect("Failed to get block hash");
-
-            // let version = (tx.version.0 as u32).to_le_bytes();
-
-            // let block = extended_rpc
-            //     .client
-            //     .get_block(&block_hash)
-            //     .await
-            //     .expect("Failed to get block");
-
-            // let block_height = block
-            //     .bip34_block_height()
-            //     .expect("Failed to get block height");
-
-            // let (index, merkle_proof) =
-            //     get_block_merkle_proof(block, *tx_id).expect("Failed to get block merkle proof");
-
-            // let vin: Vec<u8> = tx
-            //     .input
-            //     .iter()
-            //     .map(|input| {
-            //         let mut encoded_input = Vec::new();
-            //         let mut previous_output = Vec::new();
-            //         input
-            //             .previous_output
-            //             .consensus_encode(&mut previous_output)
-            //             .expect("Failed to encode previous output");
-            //         let mut script_sig = Vec::new();
-            //         input
-            //             .script_sig
-            //             .consensus_encode(&mut script_sig)
-            //             .expect("Failed to encode script sig");
-            //         let mut sequence = Vec::new();
-            //         input
-            //             .sequence
-            //             .consensus_encode(&mut sequence)
-            //             .expect("Failed to encode sequence");
-
-            //         encoded_input.extend(previous_output);
-            //         encoded_input.extend(script_sig);
-            //         encoded_input.extend(sequence);
-
-            //         Ok::<Vec<u8>, BridgeError>(encoded_input)
-            //     })
-            //     .collect::<Result<Vec<_>, _>>()
-            //     .expect("Failed to encode input")
-            //     .into_iter()
-            //     .flatten()
-            //     .collect::<Vec<u8>>();
-
-            // let vin = [vec![tx.input.len() as u8], vin].concat();
-
-            // let vout: Vec<u8> = tx
-            //     .output
-            //     .iter()
-            //     .map(|param| {
-            //         let mut raw = Vec::new();
-            //         param
-            //             .consensus_encode(&mut raw)
-            //             .map_err(|e| eyre::eyre!("Can't encode param: {}", e))?;
-
-            //         Ok::<Vec<u8>, BridgeError>(raw)
-            //     })
-            //     .collect::<Result<Vec<_>, _>>()
-            //     .expect("Failed to encode output")
-            //     .into_iter()
-            //     .flatten()
-            //     .collect::<Vec<u8>>();
-            // let vout = [vec![tx.output.len() as u8], vout].concat();
-
-            // let witness: Vec<u8> =
-            //     tx.input
-            //         .iter()
-            //         .map(|param| {
-            //             let mut raw = Vec::new();
-            //             param.witness.consensus_encode(&mut raw).map_err(|e| {
-            //                 eyre::eyre!("Can't encode param: {}", e)
-            //             })?;
-
-            //             Ok::<Vec<u8>, BridgeError>(raw)
-            //         })
-            //         .collect::<Result<Vec<_>, _>>()
-            //         .expect("Failed to encode witness")
-            //         .into_iter()
-            //         .flatten()
-            //         .collect::<Vec<u8>>();
-
-            // let lock_time = tx.lock_time.to_consensus_u32();
-
-            // unimplemented!()
-            // println!("Transaction params: {:?}", tx_params);
+        AggregatorCommands::InternalGetEmergencyStopTx { move_txids } => {
+            let move_txids = move_txids
+                .split(',')
+                .map(|txid| Txid::from_str(txid).expect("Failed to parse txid"))
+                .collect::<Vec<Txid>>();
+            let emergency_stop_tx = aggregator
+                .internal_create_emergency_stop_tx(Request::new(
+                    clementine::CreateEmergencyStopTxRequest {
+                        txids: move_txids
+                            .clone()
+                            .into_iter()
+                            .map(|txid| clementine::Txid {
+                                txid: txid.to_byte_array().to_vec(),
+                            })
+                            .collect(),
+                    },
+                ))
+                .await
+                .expect("Failed to make a request");
+            println!("Emergency stop tx: {:?}", emergency_stop_tx);
+            for (i, tx) in emergency_stop_tx
+                .into_inner()
+                .encrypted_emergency_stop_txs
+                .iter()
+                .enumerate()
+            {
+                println!(
+                    "Emergency stop tx {} for move tx {}: {}",
+                    i,
+                    move_txids[i],
+                    hex::encode(tx)
+                );
+            }
         }
         AggregatorCommands::GetReplacementDepositAddress {
             move_txid,
