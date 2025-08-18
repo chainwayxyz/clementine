cdaf4742002ed9d2ebb335f9ace348852bc7e155
aoengin <85455415+aoengin@users.noreply.github.com>
2025-08-18 11:25:00 +0000

    Deploying to gh-pages from @ chainwayxyz/clementine@431b3de6b8fb15a3696f762925e65c262b65dc6c 🚀

--- circuits_lib/bridge_circuit/lc_proof/sidebar-items.js
+++ circuits_lib/bridge_circuit/lc_proof/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["LC_IMAGE_ID"],"fn":["check_method_id","lc_proof_verifier"]};
+window.SIDEBAR_ITEMS = {"constant":["LC_IMAGE_ID"],"fn":["check_method_id","deserialize_circuit_output","is_regtest","lc_proof_verifier"]};
