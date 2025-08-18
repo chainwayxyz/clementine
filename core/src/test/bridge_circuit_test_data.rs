02e389bc7ad0fa8e95f2ae2a4e0cdecc00ccbbb5
Mehmet Efe Akca <efeakca@gmail.com>
2025-08-18 12:19:32 +0000

    fmt

--- core/src/test/bridge_circuit_test_data.rs
+++ core/src/test/bridge_circuit_test_data.rs
@@ -178,8 +178,7 @@ impl TestCase for BridgeCircuitTestData {
 #[tokio::test]
 #[ignore = "Only run this test manually, it's for data generation purposes"]
 async fn bridge_circuit_test_data_diverse_hcp_lengths() -> Result<()> {
-    initialize_logger(None)
-        .expect("Failed to initialize logger");
+    initialize_logger(None).expect("Failed to initialize logger");
     std::env::set_var("CITREA_DOCKER_IMAGE", crate::test::CITREA_E2E_DOCKER_IMAGE);
     let bridge_circuit_test_data = BridgeCircuitTestData {
         variant: BridgeCircuitTestDataVariant::Valid,
@@ -190,8 +189,7 @@ async fn bridge_circuit_test_data_diverse_hcp_lengths() -> Result<()> {
 #[tokio::test]
 #[ignore = "Only run this test manually, it's for data generation purposes"]
 async fn bridge_circuit_test_data_insuff_total_work_diverse_hcp_lens() -> Result<()> {
-    initialize_logger(None)
-        .expect("Failed to initialize logger");
+    initialize_logger(None).expect("Failed to initialize logger");
     std::env::set_var("CITREA_DOCKER_IMAGE", crate::test::CITREA_E2E_DOCKER_IMAGE);
 
     let bridge_circuit_test_data = BridgeCircuitTestData {
@@ -215,8 +213,7 @@ async fn bridge_circuit_test_data_diverse_hcp_lens_first_two_valid() -> Result<(
 #[tokio::test]
 #[ignore = "Only run this test manually, it's for data generation purposes"]
 async fn challenge_tx_with_annex() -> Result<()> {
-    initialize_logger(None)
-        .expect("Failed to initialize logger");
+    initialize_logger(None).expect("Failed to initialize logger");
     std::env::set_var("CITREA_DOCKER_IMAGE", crate::test::CITREA_E2E_DOCKER_IMAGE);
     let watchtower_challenge_tx_variant = BridgeCircuitTestData {
         variant: BridgeCircuitTestDataVariant::WithAnnex,
