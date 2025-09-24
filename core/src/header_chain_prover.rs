6144116e3d43514db3b72ec7c2b3ae129638cd7e
Mehmet Efe Akça <13402668+mmtftr@users.noreply.github.com>
2025-09-24 16:19:04 +0300

    Run proving tasks inside `spawn_blocking` (header chain and bridge circuit) (#1148)
    * feat: run proving jobs in spawn_blocking to unblock tokio sched
    
    * fix network param

--- core/src/header_chain_prover.rs
+++ core/src/header_chain_prover.rs
@@ -1028,7 +1028,7 @@ mod tests {
             .map(|header| CircuitBlockHeader::from(*header))
             .collect::<Vec<_>>();
         let receipt = HeaderChainProver::prove_block_headers(
-            Network::Regtest,
+            prover.network,
             receipt,
             block_headers[0..2].to_vec(),
         )
