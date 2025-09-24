45222b10a58942b3ae7c118311102832beda1faf
ozankaymak <ozan.kaymak@boun.edu.tr>
2025-09-24 12:10:13 +0300

    Improve error message

--- bridge-circuit-host/src/bridge_circuit_host.rs
+++ bridge-circuit-host/src/bridge_circuit_host.rs
@@ -377,7 +377,7 @@ pub fn create_spv(
 
     // Verify the SPV proof to ensure correctness
     if !spv.verify(mmr_guest) {
-        return Err(eyre!("SPV verification failed"));
+        return Err(eyre!("SPV verification failed during creation, please check inputs"));
     }
 
     Ok(spv)
