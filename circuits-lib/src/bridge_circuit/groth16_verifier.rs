6801c61cbb2ce83635d6c5906956e0bf9519d762
aoengin <ahmetoguzengin@hotmail.com>
2025-09-26 11:58:42 +0300

    fmt + clippy

--- circuits-lib/src/bridge_circuit/groth16_verifier.rs
+++ circuits-lib/src/bridge_circuit/groth16_verifier.rs
@@ -107,7 +107,7 @@ impl CircuitGroth16WithTotalWork {
 
         let prepared_vk: &[u8] = get_prepared_vk();
 
-        let prepared_vk: PreparedVerifyingKey<ark_ec::bn::Bn<ark_bn254::Config>> =
+        let prepared_vk: PreparedVerifyingKey<Bn254> =
             CanonicalDeserialize::deserialize_uncompressed(prepared_vk).unwrap();
 
         let output_digest = create_output_digest(&WorkOnlyCircuitOutput {
@@ -129,11 +129,8 @@ impl CircuitGroth16WithTotalWork {
 
         let public_inputs = vec![A0_ARK, A1_ARK, c0, c1, BN_254_CONTROL_ID_ARK];
 
-        match ark_groth16::Groth16::<Bn254>::verify_proof(&prepared_vk, &ark_proof, &public_inputs)
-        {
-            Ok(result) => result,
-            Err(_) => false,
-        }
+        ark_groth16::Groth16::<Bn254>::verify_proof(&prepared_vk, &ark_proof, &public_inputs)
+            .unwrap_or(false)
     }
 }
 
