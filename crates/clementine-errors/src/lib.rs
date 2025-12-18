522a10e983522ca83b9eb9d8272d792a555dbb08
Mehmet Efe Akça <efeakca@gmail.com>
2025-12-18 12:06:50 +0000

    refactor: remove automation feature gating in clementine-errors

--- crates/clementine-errors/src/lib.rs
+++ crates/clementine-errors/src/lib.rs
@@ -162,7 +162,6 @@ pub enum VerificationError {
 }
 
 /// Transaction sending errors.
-#[cfg(feature = "automation")]
 #[derive(Debug, Error)]
 pub enum SendTxError {
     #[error("Unconfirmed fee payer UTXOs left")]
@@ -181,7 +180,6 @@ pub enum SendTxError {
 }
 
 /// State machine errors.
-#[cfg(feature = "automation")]
 #[derive(Debug, Error)]
 pub enum StateMachineError {
     #[error("State machine received event that it doesn't know how to handle: {0}")]
@@ -224,7 +222,6 @@ pub enum BridgeError {
     // Transaction(#[from] TxError),
     #[error("Failed to build transactions: {0}")]
     Transaction(eyre::Report),
-    #[cfg(feature = "automation")]
     #[error("Failed to send transactions: {0}")]
     SendTx(#[from] SendTxError),
     #[error("Aggregator error: {0}")]
@@ -235,7 +232,6 @@ pub enum BridgeError {
     SpendableTxIn(#[from] SpendableTxInError),
     #[error("Bitcoin RPC error: {0}")]
     BitcoinRPC(#[from] BitcoinRPCError),
-    #[cfg(feature = "automation")]
     #[error("State machine error: {0}")]
     StateMachine(#[from] StateMachineError),
     #[error("RPC authentication error: {0}")]
