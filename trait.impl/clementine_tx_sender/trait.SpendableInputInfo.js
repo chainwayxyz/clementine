e621292bb7b364a80dd2960369ee6628fa2154ee
atacann <111396231+atacann@users.noreply.github.com>
2026-04-29 15:13:08 +0000

    Deploying to gh-pages from @ chainwayxyz/clementine@b1854944ccc108f93d1d08767fd2bdf43d93cf8a 🚀

--- trait.impl/clementine_tx_sender/trait.SpendableInputInfo.js
+++ trait.impl/clementine_tx_sender/trait.SpendableInputInfo.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["clementine_core",[["impl <a class=\"trait\" href=\"clementine_tx_sender/trait.SpendableInputInfo.html\" title=\"trait clementine_tx_sender::SpendableInputInfo\">SpendableInputInfo</a> for <a class=\"struct\" href=\"clementine_core/builder/transaction/input/struct.SpendableTxIn.html\" title=\"struct clementine_core::builder::transaction::input::SpendableTxIn\">SpendableTxIn</a>"]]],["clementine_tx_sender",[]]]);
+    var implementors = Object.fromEntries([["clementine_core",[["impl SpendableInputInfo for <a class=\"struct\" href=\"clementine_core/builder/transaction/input/struct.SpendableTxIn.html\" title=\"struct clementine_core::builder::transaction::input::SpendableTxIn\">SpendableTxIn</a>"]]],["clementine_tx_sender",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[385,28]}
+//{"start":57,"fragment_lengths":[245,28]}
--- trait.impl/clementine_tx_sender/trait.TxSenderTxBuilder.js
+++ trait.impl/clementine_tx_sender/trait.TxSenderTxBuilder.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["clementine_core",[["impl <a class=\"trait\" href=\"clementine_tx_sender/trait.TxSenderTxBuilder.html\" title=\"trait clementine_tx_sender::TxSenderTxBuilder\">TxSenderTxBuilder</a> for <a class=\"struct\" href=\"clementine_core/tx_sender_ext/struct.CoreTxBuilder.html\" title=\"struct clementine_core::tx_sender_ext::CoreTxBuilder\">CoreTxBuilder</a>"]]]]);
+    var implementors = Object.fromEntries([["clementine_core",[["impl TxSenderTxBuilder for <a class=\"struct\" href=\"clementine_core/tx_sender_ext/struct.CoreTxBuilder.html\" title=\"struct clementine_core::tx_sender_ext::CoreTxBuilder\">CoreTxBuilder</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[356]}
+//{"start":57,"fragment_lengths":[218]}
