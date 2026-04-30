dfc92185d2d8e4a50bde7adcb18d8dacefef8f12
atacan <atacaniyidogan@gmail.com>
2026-04-30 17:25:23 +0200

    fix migrations

--- core/src/database/migrations/0001_add_last_processed_lcp.down.sql
+++
@@ -1,2 +0,0 @@
--- Remove last_processed_lcp column from state_manager_status table
-ALTER TABLE state_manager_status DROP COLUMN IF EXISTS last_processed_lcp;
--- core/src/database/migrations/0001_add_last_processed_lcp.up.sql
+++
@@ -1,3 +0,0 @@
--- Add last_processed_lcp column to state_manager_status table
-ALTER TABLE state_manager_status
-ADD COLUMN IF NOT EXISTS last_processed_lcp INT DEFAULT NULL;
